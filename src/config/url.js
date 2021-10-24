aba97752bca2a939692050425688ae47f6978688
Taras Filatov <phwizard@gmail.com>
2021-10-25 00:15:13 +0100

    Update url.js

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,9 @@
+/*
+Copyright 2019-2021 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+*/
+
 const urlDefault = "https://app-dev.dappros.com/v1/";
 const urlDefaultPort7777 = "https://app-dev.dappros.com:7777/api/v1/"
 
