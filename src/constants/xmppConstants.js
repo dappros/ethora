ea5621c5f9dc38361d2296c4782d4828951ba343
Taras Filatov <phwizard@gmail.com>
2021-10-25 00:16:05 +0100

    Update xmppConstants.js

--- src/constants/xmppConstants.js
+++ src/constants/xmppConstants.js
@@ -1,3 +1,9 @@
+/*
+Copyright 2019-2021 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+*/
+
 export const SERVICE = "wss://dev.dxmpp.com:5443/ws";
 export const DOMAIN = "dev.dxmpp.com";
 export const CONFERENCEDOMAIN = "@conference.dev.dxmpp.com";
