0c4f5fb56e3596f6d5e9743acf4a25c01e316d81
Taras Filatov <phwizard@gmail.com>
2021-10-25 00:26:55 +0100

    Update openChatFromChatLink.js

--- src/helpers/openChatFromChatLink.js
+++ src/helpers/openChatFromChatLink.js
@@ -1,3 +1,9 @@
+/*
+Copyright 2019-2021 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+*/
+
 import { underscoreManipulation } from "./underscoreLogic";
 import {
     subscribeAndOpenChat,
