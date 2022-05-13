7c737d1caf1f8ab6e5072e5b78dde2b41a57d457
Anton Kolot <antvelkol@gmail.com>
2022-05-13 18:20:51 +0300

    Added bot message logic and connected handlers

--- bots/huthut/router.js
+++ bots/huthut/router.js
@@ -1,5 +1,12 @@
-import {connectRoom, messageCheck, sendMessage} from "./actions.js";
-import messages from "./config/messages.js";
+import {connectRoom, messageCheck, userSteps} from "./actions.js";
+import {testHandler} from "./handlers/test.js";
+import {backTurnForestHandler} from "./handlers/backTurnForest.js";
+import {helpHandler} from "./handlers/help.js";
+import {leaveHandler} from "./handlers/leave.js";
+import {frontTurnMe} from "./handlers/frontTurnMe.js";
+import {errorHandler} from "./handlers/error.js";
+import {storeItemHandler} from "./handlers/storeItem.js";
+import {searchItemsHandler} from "./handlers/searchItems.js";
 
 const router = (xmpp, message, sender, receiver, requestType) => {
     if (requestType === 'x' && message.match(/\binvite\S*\b/g)) {
@@ -8,17 +15,42 @@ const router = (xmpp, message, sender, receiver, requestType) => {
     }
 
     if (requestType === 'body') {
-        console.log('=> Message received from ', receiver, message)
-        if (messageCheck(message, 'hut test')) {
-            sendMessage(xmpp, receiver, 'message', messages.testMessage)
+        let userStep = userSteps('getStep', sender, null);
+        //actions that are performed in the first step, when the bot does not yet know what the user wants
+        if (userStep === 1) {
+            if (messageCheck(message, 'hut test')) {
+                testHandler(xmpp, sender, receiver, message);
+            } else if (messageCheck(message, 'hut back turn forest')) {
+                backTurnForestHandler(xmpp, sender, receiver, message);
+            } else if (messageCheck(message, 'hut') || messageCheck(message, 'hut help')) {
+                helpHandler(xmpp, sender, receiver, message, userStep);
+            }
         }
 
-        if (messageCheck(message, 'hut back turn forest')) {
-            sendMessage(xmpp, receiver, 'message', messages.visiteingHut.firstGreeting)
+        if (userStep === 2) {
+            if (messageCheck(message, 'hut front turn me')) {
+                frontTurnMe(xmpp, sender, receiver, message);
+            }else if(messageCheck(message, 'hut') || messageCheck(message, 'hut help')){
+                helpHandler(xmpp, sender, receiver, message, userStep);
+            }
         }
 
-        if (messageCheck(message, 'hut front turn me')) {
-            sendMessage(xmpp, receiver, 'message', messages.visiteingHut.openingHut)
+        if (userStep === 3) {
+            if(Number.isInteger(Number(message)) && message <= 3){
+                if(message === 1){
+                    storeItemHandler(xmpp, sender, receiver, message);
+                }else if(message === 2){
+                    searchItemsHandler(xmpp, sender, receiver, message);
+                }else{
+                    leaveHandler(xmpp, sender, receiver, message, userStep);
+                }
+            }else{
+                errorHandler(xmpp, sender, receiver, message, userStep);
+            }
+        }
+
+        if (messageCheck(message, 'hut close') || messageCheck(message, 'hut leave')) {
+            leaveHandler(xmpp, sender, receiver, message, userStep);
         }
 
     }
