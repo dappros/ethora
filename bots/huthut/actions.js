dfdd757a803c6f3bec96a8abfcf69dd2fb9d58bd
Anton Kolot <antvelkol@gmail.com>
2022-05-17 19:41:27 +0300

    fast hardcode fix

--- bots/huthut/actions.js
+++ bots/huthut/actions.js
@@ -9,7 +9,8 @@ let userStepsList = [];
 // For a private send, the "type" attribute must change to "message". To send to the chat room "groupchat"
 const sendMessage = (xmpp, jid, type, message, receiverData) => {
     xmpp.send(xml('message', {
-        to: receiverData ? receiverData.attrs.roomJid : jid,
+        // to: receiverData ? receiverData.attrs.roomJid : jid,
+        to: 'f6b35114579afc1cb5dbdf5f19f8dac8971a90507ea06083932f04c50f26f1c5@conference.dev.dxmpp.com',
         type: 'groupchat',
         id: "sendMessage"
     }, xml('data', {
@@ -17,7 +18,10 @@ const sendMessage = (xmpp, jid, type, message, receiverData) => {
         senderFirstName: botOptions.botData.firstName,
         senderLastName: botOptions.botData.lastName,
         photoURL: botOptions.botData.photoURL
-    }), xml('body', {}, receiverData ? receiverData.attrs.senderFirstName+': '+message : message)));
+    }), xml('body', {}, receiverData.attrs.senderFirstName ? receiverData.attrs.senderFirstName+': '+message : message)));
+// }), xml('body', {}, receiverData ? receiverData.attrs.senderFirstName+': '+message : message)));
+
+return true;
 }
 
 const connectRoom = (xmpp, address, roomAddress) => {
@@ -50,11 +54,11 @@ const messageCheck = (str, keywords) => {
 }
 
 const userSteps = (type, jid, newStep) => {
-    console.log('=>=> Run user steps, find user. Type: ', type, ' user jid: ', jid);
+    // console.log('=>=> Run user steps, find user. Type: ', type, ' user jid: ', jid);
     let userIndex = userStepsList.findIndex(user => user.name === jid);
 
     if (userIndex < 0) {
-        console.log('=>=> Create user step', jid);
+        // console.log('=>=> Create user step', jid);
         userStepsList.push({name: jid, step: 1});
         return 1;
     }
@@ -64,7 +68,7 @@ const userSteps = (type, jid, newStep) => {
     }
 
     if (type === 'setStep') {
-        console.log('=>=> Set new step for user ', jid)
+        // console.log('=>=> Set new step for user ', jid)
         userStepsList[userIndex].step = newStep;
         return true;
     }
