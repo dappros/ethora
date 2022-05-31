613e7a129ef931cd384cd44d6a5101319b41eef6
Anton Kolot <antvelkol@gmail.com>
2022-05-31 11:09:24 +0300

    Added new steps for processing help

--- bots/huthut/handlers/help.js
+++ bots/huthut/handlers/help.js
@@ -3,11 +3,46 @@ import messages from "../config/messages.js";
 
 export const helpHandler = (data) => {
     console.log('=> helpHandler | Message received from ', data.receiver, data.message);
-    return sendMessage(
-        data,
-        data.userStep === 1 ? messages.help.whereToBegin : messages.help.secondStep,
-        'message',
-        false,
-        0,
-    );
+
+    if(data.userStep === 1){
+        return sendMessage(
+            data,
+            messages.help.whereToBegin,
+            'message',
+            false,
+            0,
+        );
+    }
+
+    if(data.userStep === 2){
+        return sendMessage(
+            data,
+            messages.help.secondStep,
+            'message',
+            false,
+            0,
+        );
+    }
+
+    if(data.userStep === 4){
+        return sendMessage(
+            data,
+            messages.visitingHut.toPlaceItemNeed,
+            'message',
+            false,
+            0,
+        );
+    }
+
+    if(data.userStep === 5){
+        return sendMessage(
+            data,
+            messages.visitingHut.toGetItemNeed,
+            'message',
+            false,
+            0,
+        );
+    }
+
+
 }
