f7ecc825b3f39a09386a0bb9c215caa73d04b605
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-05-31 13:57:07 +0300

    changed transaction retreiving

--- src/screens/TransactionsScreen.tsx
+++ src/screens/TransactionsScreen.tsx
@@ -1,5 +1,5 @@
 import {observer} from 'mobx-react-lite';
-import { View } from 'native-base';
+import {View} from 'native-base';
 import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import SecondaryHeader from '../components/SecondaryHeader/SecondaryHeader';
@@ -9,13 +9,11 @@ import {useStores} from '../stores/context';
 const TransactionsScreen = observer(() => {
   const {walletStore, loginStore} = useStores();
   useEffect(() => {
-    walletStore.fetchTransaction(
-      loginStore.initialData.walletAddress,
-      loginStore.userToken,
-      true,
-      10,
-      0
-    )
+    walletStore.fetchOwnTransactions(
+      loginStore.walletAddress,
+      walletStore.limit,
+      walletStore.offset,
+    );
     return () => {};
   }, []);
 
@@ -23,22 +21,17 @@ const TransactionsScreen = observer(() => {
     <View flex={1}>
       <SecondaryHeader title="Transactions" />
       <TransactionsList
-      transactions={walletStore.transactions}
-      onEndReached={() => {
-        if (
-          walletStore.transactions.length < walletStore.total
-        ) {
-
-          walletStore.fetchTransaction(
-            loginStore.initialData.walletAddress,
-            loginStore.userToken,
-            false,
-            walletStore.limit,
-            walletStore.offset + walletStore.limit
-          )
-        }
-      }}
-      walletAddress={loginStore.initialData.walletAddress}
+        transactions={walletStore.transactions.slice().reverse()}
+        onEndReached={() => {
+          if (walletStore.transactions.length < walletStore.total) {
+            walletStore.fetchOwnTransactions(
+              loginStore.walletAddress,
+              walletStore.limit,
+              walletStore.offset,
+            );
+          }
+        }}
+        walletAddress={loginStore.initialData.walletAddress}
       />
     </View>
   );
