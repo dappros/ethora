a2a3324fe33f9e4585c35a0c2abfbb54a432d7e9
Anton Kolot <antvelkol@gmail.com>
2022-05-31 11:11:34 +0300

    Added logic for item placement

--- bots/huthut/handlers/storeItem.js
+++ bots/huthut/handlers/storeItem.js
@@ -1,8 +1,18 @@
 import {sendMessage, userSteps} from "../actions.js";
 import messages from "../config/messages.js";
+import botOptions from "../config/config.js";
 
 export const storeItemHandler = (data) => {
     console.log('=> storeItemHandler | Message received from ', data.receiver, data.message);
+    if(data.receiverData.attrs.tokenAmount < botOptions.botData.storeFee){
+        return sendMessage(
+            data,
+            messages.errors.payment.wrongAmount+': '+botOptions.botData.storeFee,
+            'message',
+            false,
+            0,
+        );
+    }
     userSteps('setStep', data.sender, 1);
     return sendMessage(
         data,
