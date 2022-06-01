3ae6e4fc6d3d9b8ae1c4190030df8e8aac724a62
Anton Kolot <antvelkol@gmail.com>
2022-06-01 17:58:27 +0300

    Added a function for coin transfer - as an example

--- bots/huthut/handlers/transferCoin.js
+++ bots/huthut/handlers/transferCoin.js
@@ -1,57 +1,25 @@
-import {sendMessage, userSteps} from "../actions.js";
+import {sendMessage} from "../actions.js";
 import botOptions from "../config/config.js";
 import messages from "../config/messages.js";
-import {getRandomItem, transferNft} from "../api.js";
+import {transferToken} from "../api.js";
 import {sendErrorMessage} from "./error.js";
 
 export const transferCoinHandler = (data) => {
     console.log('=> transferCoinHandler | Message received from ', data.receiver, data.message);
-    if(data.receiverData.attrs.tokenAmount < botOptions.botData.collectFee){
-        return sendMessage(
-            data,
-            messages.errors.payment.wrongAmount+': '+botOptions.botData.collectFee,
-            'message',
-            false,
-            0,
-        );
-    }
-
     let tokenAmount = 1;
 
-    getRandomItem().then((response) => {
-        userSteps('setStep', data.sender, 1);
-        if (response) {
-            sendMessage(
-                data,
-                messages.visitingHut.randomlyGotItem + response.tokenName,
-                'message',
-                false,
-                0,
-            );
-            transferNft(response.nftId, data.receiverData.attrs.senderWalletAddress, tokenAmount).then(responseNft => {
-                console.log('=> Transfer success', responseNft);
-                return sendMessage(
-                    data,
-                    botOptions.botData.firstName + ' ' + botOptions.botData.lastName + ' -> ' + tokenAmount + ' ' + response.tokenName + ' -> ' + data.receiverData.attrs.senderFirstName,
-                    'message',
-                    true,
-                    tokenAmount,
-                );
-            }).catch((error) => {
-                console.log('transferNft ERROR', error);
-                return sendErrorMessage(data, messages.errors.payment.transferTokenError)
-            })
-        } else {
-            return sendMessage(
-                data,
-                messages.visitingHut.noItemsInBot,
-                'message',
-                false,
-                0,
-            );
-        }
-    }).catch((err) => {
-        console.log('ERROR', err)
-        sendErrorMessage(data, messages.errors.payment.getRandomItemError)
+    transferToken(botOptions.botData.tokenSymbol, botOptions.botData.tokenName, tokenAmount, data.receiverData.attrs.senderWalletAddress).then(response => {
+        console.log('=> Transfer success', response.data);
+        return sendMessage(
+            data,
+            botOptions.botData.firstName + ' ' + botOptions.botData.lastName + ' -> ' + tokenAmount + ' ' + botOptions.botData.tokenName + ' -> ' + data.receiverData.attrs.senderFirstName,
+            'message',
+            true,
+            tokenAmount,
+        );
+    }).catch((error) => {
+        console.log('transferToken ERROR', error)
+        sendErrorMessage(data, messages.errors.payment.transferTokenError)
     })
+
 }
