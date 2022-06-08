a9c86e35a7714856baf075bea08931aa03358729
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-06-08 13:55:03 +0300

    New mimetype for modal

--- src/components/Chat/ImageMessage.tsx
+++ src/components/Chat/ImageMessage.tsx
@@ -1,6 +1,7 @@
 import {Box, Image} from 'native-base';
 import React from 'react';
 import {View} from 'react-native';
+import FastImage from 'react-native-fast-image';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {heightPercentageToDP} from 'react-native-responsive-screen';
 import {formatBytes} from '../../helpers/chat/formatBytes';
@@ -28,11 +29,9 @@ export const ImageMessage = ({url, size, onLongPress, onPress}:ImageMessageProps
       <MessageSize unit={formatedSize.unit} size={formatedSize.size} />
 
       <Box p={'1.5'}>
-        <Image
-          height={heightPercentageToDP('22%')}
-          width={heightPercentageToDP('22%')}
+        <FastImage
+          style={{width: heightPercentageToDP('22%'), height: heightPercentageToDP('22%'), borderRadius: 10}}
           source={{uri: url}}
-          rounded={'md'}
           alt={'Image message'}
         />
       </Box>
--- src/components/Modals/ChatMediaModal.js
+++ src/components/Modals/ChatMediaModal.js
@@ -1,12 +1,37 @@
 import {Box, Image, Modal} from 'native-base';
 import React from 'react';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
+import { imageMimetypes, videoMimetypes } from '../../constants/mimeTypes';
+import VideoPlayer from 'react-native-video-player';
+import { TouchableOpacity } from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export const ChatMediaModal = ({open, url, type, onClose}) => {
   const renderModalContent = () => {
-    if (type === 'image/jpeg' || type === 'image/png') {
+    if (imageMimetypes[type]) {
       return (
         <Image alt="image" source={{uri: url}} w={'100%'} height={'100%'} />
+      );
+    }
+    if (videoMimetypes[type]) {
+      return (
+        <TouchableOpacity
+          // onPress={() => setVideoPaused(prev => !prev)}
+          activeOpacity={1}
+          style={{height: hp('100%'), width: '100%'}}>
+          <VideoPlayer
+            video={{
+              uri: url,
+            }}
+            autoplay
+            videoWidth={wp('100%')}
+            videoHeight={hp('100%')}
+            // thumbnail={{uri: 'https://i.picsum.photos/id/866/1600/900.jpg'}}
+          />
+        </TouchableOpacity>
       );
     }
     return null;
