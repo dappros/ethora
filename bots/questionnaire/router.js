142848dd73c1ae1182341ea34fe8cf93c7960fdd
Anton Kolot <antvelkol@gmail.com>
2022-06-16 21:16:55 +0300

    Added handlers for questions and answers, as well as validation for answers

--- bots/questionnaire/router.js
+++ bots/questionnaire/router.js
@@ -3,6 +3,8 @@ import {testHandler} from "./handlers/test.js";
 import {helpHandler} from "./handlers/help.js";
 import {leaveHandler} from "./handlers/leave.js";
 import messages from "./config/messages.js";
+import {questionHandler} from "./handlers/question.js";
+import {answerHandler} from "./handlers/answer.js";
 
 const router = (xmpp, message, sender, receiver, requestType, receiverData, stanzaId) => {
     if (requestType === 'body') {
@@ -22,7 +24,15 @@ const router = (xmpp, message, sender, receiver, requestType, receiverData, stan
             if (messageCheck(message, 'bot test')) {
                 return testHandler(handlerData);
             }
+            if (messageCheck(message, 'bot')) {
+                return questionHandler(handlerData);
+            }
+        }
 
+        if(userStep === 2) {
+            if (messageCheck(message, 'bot') && message.length > 3 && message.split(' ', 1)[0].toUpperCase() === 'BOT') {
+                return answerHandler(handlerData);
+            }
         }
 
         //Global message handlers not associated with steps
