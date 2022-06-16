31df641799419e7d39d21640cd95ef327349af5c
Anton Kolot <antvelkol@gmail.com>
2022-06-16 21:13:27 +0300

    Added date to response model

--- bots/questionnaire/models/answers.js
+++ bots/questionnaire/models/answers.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const Answers = new Schema({
     question_id: {type: Number, required: true},
     answer: {type: String, required: true},
-    user_jid: {type: String, required: true, index: true}
+    user_jid: {type: String, required: true, index: true},
+    timestamp: { type: Date, default: Date.now}
 });
 
 export const Answer = mongoose.model('Answers', Answers);
