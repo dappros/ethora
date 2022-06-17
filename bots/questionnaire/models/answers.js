3ecb9938b64b6d0f8d987567368908c5b11340eb
Anton Kolot <antvelkol@gmail.com>
2022-06-17 16:47:51 +0300

    Added appId to model

--- bots/questionnaire/models/answers.js
+++ bots/questionnaire/models/answers.js
@@ -6,7 +6,8 @@ const Answers = new Schema({
     question_id: {type: Number, required: true},
     answer: {type: String, required: true},
     user_jid: {type: String, required: true, index: true},
-    timestamp: { type: Date, default: Date.now}
+    timestamp: { type: Date, default: Date.now},
+    app_id: {type: String, required: true, index: true},
 });
 
 export const Answer = mongoose.model('Answers', Answers);
