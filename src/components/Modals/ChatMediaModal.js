6e398c5fca9eaec6ab951047dc3c750f8ac22e26
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-06-22 15:41:57 +0300

    Changed close button

--- src/components/Modals/ChatMediaModal.js
+++ src/components/Modals/ChatMediaModal.js
@@ -1,19 +1,27 @@
 import {Box, Image, Modal} from 'native-base';
 import React from 'react';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
-import { imageMimetypes, videoMimetypes } from '../../constants/mimeTypes';
+import {imageMimetypes, videoMimetypes} from '../../constants/mimeTypes';
 import VideoPlayer from 'react-native-video-player';
-import { TouchableOpacity } from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
+import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+
 export const ChatMediaModal = ({open, url, type, onClose}) => {
   const renderModalContent = () => {
     if (imageMimetypes[type]) {
       return (
-        <Image alt="image" resizeMode={'contain'} source={{uri: url}} w={'100%'} height={'100%'} />
+        <Image
+          alt="image"
+          resizeMode={'contain'}
+          source={{uri: url}}
+          w={'100%'}
+          height={'100%'}
+        />
       );
     }
     if (videoMimetypes[type]) {
@@ -44,7 +52,9 @@ export const ChatMediaModal = ({open, url, type, onClose}) => {
         bg: 'black',
       }}>
       <Box w={widthPercentageToDP('90%')} h="90%">
-        <Modal.CloseButton color={'white'} />
+        <TouchableOpacity style={{position: 'absolute', right: 0, zIndex: 9999}} onPress={onClose}>
+          <AntDesignIcons name="close" size={30} color={'white'} />
+        </TouchableOpacity>
         {renderModalContent()}
       </Box>
     </Modal>
