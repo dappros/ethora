79063a58e062749c211031ccbf2b2b94abd2a541
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-06-27 16:06:09 +0300

    alpha helper

---
+++ src/helpers/aplha.ts
@@ -0,0 +1,4 @@
+export function alpha(color: string, opacity: number) {
+  const _opacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
+  return color + _opacity.toString(16).toUpperCase();
+}
