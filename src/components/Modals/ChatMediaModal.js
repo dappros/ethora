1250dd6b90af557b94f3ec1c5269d9d1ce2f4af2
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-06-27 16:05:33 +0300

    Calcucating media height

--- src/components/Modals/ChatMediaModal.js
+++ src/components/Modals/ChatMediaModal.js
@@ -1,43 +1,58 @@
 import {Box, Image, Modal} from 'native-base';
-import React from 'react';
+import React, {useState} from 'react';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
 import {imageMimetypes, videoMimetypes} from '../../constants/mimeTypes';
 import VideoPlayer from 'react-native-video-player';
-import {TouchableOpacity} from 'react-native';
+import {TouchableOpacity, Image as NativeImage, Dimensions} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import FastImage from 'react-native-fast-image';
+const {width, height: windowHeight} = Dimensions.get('window');
 
 export const ChatMediaModal = ({open, url, type, onClose}) => {
+  const [height, setHeight] = useState(0);
   const renderModalContent = () => {
     if (imageMimetypes[type]) {
       return (
-        <Image
-          alt="image"
-          resizeMode={'contain'}
-          source={{uri: url}}
-          w={'100%'}
-          height={'100%'}
+        <FastImage
+          style={{
+            width: '100%',
+            height: height > windowHeight - 50 ? windowHeight - 120 : height,
+          }}
+          source={{
+            uri: url,
+            priority: FastImage.priority.normal,
+          }}
+          resizeMode={FastImage.resizeMode.contain}
+          onLoad={evt =>
+            setHeight(
+              (evt.nativeEvent.height / evt.nativeEvent.width) * width, // By this, you keep the image ratio
+            )
+          }
         />
       );
     }
     if (videoMimetypes[type]) {
       return (
         <TouchableOpacity
-          // onPress={() => setVideoPaused(prev => !prev)}
           activeOpacity={1}
-          style={{height: hp('100%'), width: '100%'}}>
+          style={{height: height, width: '100%'}}>
           <VideoPlayer
             video={{
               uri: url,
             }}
             autoplay
             videoWidth={wp('100%')}
-            videoHeight={hp('100%')}
-            // thumbnail={{uri: 'https://i.picsum.photos/id/866/1600/900.jpg'}}
+            videoHeight={height}
+            onLoad={e =>
+              setHeight(
+                (e.naturalSize.height / e.naturalSize.width) * width, // By this, you keep the image ratio
+              )
+            }
           />
         </TouchableOpacity>
       );
@@ -51,8 +66,10 @@ export const ChatMediaModal = ({open, url, type, onClose}) => {
       _backdrop={{
         bg: 'black',
       }}>
-      <Box w={widthPercentageToDP('90%')} h="90%">
-        <TouchableOpacity style={{position: 'absolute', right: 0, zIndex: 9999}} onPress={onClose}>
+      <Box w={widthPercentageToDP('90%')}>
+        <TouchableOpacity
+          style={{position: 'absolute', right: 0, top: -30, zIndex: 9999}}
+          onPress={onClose}>
           <AntDesignIcons name="close" size={30} color={'white'} />
         </TouchableOpacity>
         {renderModalContent()}
