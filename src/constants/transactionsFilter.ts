8c6fe124d758b72cabf65988479baa49859fdf88
Taras Filatov <phwizard@gmail.com>
2022-07-07 06:02:51 +0100

    Update transactionsFilter.ts

--- src/constants/transactionsFilter.ts
+++ src/constants/transactionsFilter.ts
@@ -1,3 +1,10 @@
+/*
+Copyright 2019-2022 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+Note: linked open-source libraries and components may be subject to their own licenses.
+*/
+
 export const FILTERS = {
     sent: 'sent',
     all: 'all',
