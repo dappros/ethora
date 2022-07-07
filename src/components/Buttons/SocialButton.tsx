47a38c46066bf416aff13a12fbfe2fd48b0497be
Taras Filatov <phwizard@gmail.com>
2022-07-07 05:45:29 +0100

    Update SocialButton.tsx

--- src/components/Buttons/SocialButton.tsx
+++ src/components/Buttons/SocialButton.tsx
@@ -1,3 +1,10 @@
+/*
+Copyright 2019-2022 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+Note: linked open-source libraries and components may be subject to their own licenses.
+*/
+
 import { Button, Text } from 'native-base';
 import { ColorType } from 'native-base/lib/typescript/components/types';
 import * as React from 'react';
