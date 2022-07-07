bbc237a6f46995e711bae768ea336092436a892f
Taras Filatov <phwizard@gmail.com>
2022-07-07 05:55:15 +0100

    Update RoomsTabBar.tsx

--- src/components/RoomList/RoomsTabBar.tsx
+++ src/components/RoomList/RoomsTabBar.tsx
@@ -1,3 +1,10 @@
+/*
+Copyright 2019-2022 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+Note: linked open-source libraries and components may be subject to their own licenses.
+*/
+
 import React, {useMemo, useState} from 'react';
 import {observer} from 'mobx-react-lite';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
