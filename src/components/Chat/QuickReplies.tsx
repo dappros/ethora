181d8b735652a44f73bed48f12fbe99995172fee
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-08-17 20:33:08 +0300

    Value for bot

--- src/components/Chat/QuickReplies.tsx
+++ src/components/Chat/QuickReplies.tsx
@@ -1,7 +1,7 @@
 /*
 Copyright 2019-2022 (c) Dappros Ltd, registered in England & Wales, registration number 11455432. All rights reserved.
 You may not use this file except in compliance with the License.
-You may obtain a copy of the License at https://github.com/dappros/ethora/blob/main/LICENSE.
+You may obtain a copy of the License at https://github.com/dappros/pericon/blob/main/LICENSE.
 Note: linked open-source libraries and components may be subject to their own licenses.
 */
 
@@ -10,7 +10,7 @@ import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
 import {commonColors} from '../../../docs/config';
-import { colors } from '../../constants/messageColors';
+import {colors} from '../../constants/messageColors';
 import {alpha} from '../../helpers/aplha';
 import {underscoreManipulation} from '../../helpers/underscoreLogic';
 import {useStores} from '../../stores/context';
@@ -27,7 +27,7 @@ export const QuickReplies = ({
   const isSameUser =
     messageAuthor ===
     underscoreManipulation(loginStore.initialData.walletAddress);
-  const onQuickReplyPress = text => {
+  const onQuickReplyPress = ({name, value, notDisplayedValue}) => {
     const data = {
       senderFirstName: loginStore.initialData.firstName,
       senderLastName: loginStore.initialData.lastName,
@@ -38,36 +38,41 @@ export const QuickReplies = ({
       mucname: roomName,
       photoURL: loginStore.userAvatar,
       roomJid: roomJid,
+      notDisplayedValue,
     };
     sendMessageStanza(
       underscoreManipulation(loginStore.initialData.walletAddress),
       roomJid,
-      text,
+      value,
       data,
       chatStore.xmpp,
     );
   };
   return (
-    <View alignItems={isSameUser ? 'flex-end' : 'flex-start'} width={width}>
-      {quickReplies.map(item => {
-        return (
-          <TouchableOpacity
-            activeOpacity={0.6}
-            onPress={() => onQuickReplyPress(item.value)}
-            style={[
-              styles.button,
-              {
-                width: width - 60,
-                backgroundColor: isSameUser
-                  ? alpha(commonColors.primaryColor, 0.3)
-                  : alpha(colors.leftBubbleBackground, 0.4),
-              },
-            ]}
-            key={item.value}>
-            <Text color={'white'}>{item.name}</Text>
-          </TouchableOpacity>
-        );
-      })}
+    <View
+      alignItems={isSameUser ? 'flex-end' : 'flex-start'}
+      style={{marginVertical: 4}}
+      width={width}>
+      {!!quickReplies &&
+        quickReplies.map(item => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.6}
+              onPress={() => onQuickReplyPress(item)}
+              style={[
+                styles.button,
+                {
+                  width: width - 60,
+                  backgroundColor: isSameUser
+                    ? alpha(commonColors.primaryColor, 0.3)
+                    : alpha(colors.leftBubbleBackground, 0.4),
+                },
+              ]}
+              key={item.value}>
+              <Text color={'white'}>{item.name}</Text>
+            </TouchableOpacity>
+          );
+        })}
     </View>
   );
 };
