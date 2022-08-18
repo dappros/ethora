082081320e77f971e4a8bda7101f7798a02abf4c
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-08-18 14:14:51 +0300

    Toast messages

--- src/components/Toast/toast.ts
+++ src/components/Toast/toast.ts
@@ -7,11 +7,40 @@ Note: linked open-source libraries and components may be subject to their own li
 
 import Toast from 'react-native-toast-message';
 
-export const showToast = (type:'success'|'error', title:string, desc:string, position:'top'|'bottom') => {
-    Toast.show({
-        type:type,
-        text1:title,
-        text2:desc,
-        position:position
-    })
-}
+export const showToast = (
+  type: 'success' | 'error',
+  title: string,
+  desc: string,
+  position: 'top' | 'bottom',
+) => {
+  Toast.show({
+    type: type,
+    text1: title,
+    text2: desc,
+    position: position,
+  });
+};
+export const showError = (title: string, subtitle: string) => {
+  Toast.show({
+    type: 'error',
+    text1: title,
+    text2: subtitle,
+    position: 'bottom',
+  });
+};
+export const showInfo = (title: string, subtitle: string) => {
+  Toast.show({
+    type: 'info',
+    text1: title,
+    text2: subtitle,
+    position: 'bottom',
+  });
+};
+export const showSuccess = (title: string, subtitle: string) => {
+  Toast.show({
+    type: 'success',
+    text1: title,
+    text2: subtitle,
+    position: 'bottom',
+  });
+};
