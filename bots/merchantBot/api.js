1f6041d1a242fc62175a372e74984a3f9e6e68fc
Anton Kolot <antvelkol@gmail.com>
2022-08-19 13:02:17 +0300

    Update refresh token

--- bots/merchantBot/api.js
+++ bots/merchantBot/api.js
@@ -3,9 +3,13 @@ import botOptions from "./config/config.js";
 
 export let loginData = [];
 
+const http = axios.create({
+    baseURL: botOptions.apiUrl
+});
+
 export const botLogin = async (username, password) => {
     try {
-        const request = await axios.post(botOptions.apiUrl + 'users/login', {
+        const request = await http.post('users/login', {
             username: username,
             password: password
         }, {
@@ -15,15 +19,55 @@ export const botLogin = async (username, password) => {
             },
         });
         loginData = request.data;
+        console.log('=> Bot address: ', loginData.user.defaultWallet.walletAddress)
         return true;
     } catch (error) {
         return error.data;
     }
 }
 
-const requestTransferToken = async (tokenId, tokenName, amount, wallet) => {
+const refresh = () => {
+    return new Promise((resolve, reject) => {
+        http.post('/users/login/refresh', {}, {headers: {'Authorization': store.refreshToken}})
+            .then(response => {
+                loginData.token = response.data.token;
+                loginData.refreshToken = response.data.refreshToken;
+                resolve(response)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+}
+
+http.interceptors.response.use(undefined, error => {
+    if (!error.response || error.response.status !== 401) {
+        return Promise.reject(error)
+    }
+
+  if (error.request.path ===  '/v1/users/login/refresh') {
+      return Promise.reject(error)
+  }
+
+  const request = error.config
+
+  return refresh()
+      .then(() => {
+          return new Promise((resolve) => {
+              'sendig request after refresh'
+              request.headers['Authorization'] = loginData.token
+              resolve(http(request))
+          })
+      })
+      .catch((error) => {
+          console.log('Refresh reject')
+          return Promise.reject(error)
+      })
+})
+
+export const transferToken = async (tokenId, tokenName, amount, wallet) => {
     try {
-        let test = await axios.post(botOptions.apiUrl + 'tokens/transfer', {
+        let test = await http.post('tokens/transfer', {
             tokenId: tokenId,
             tokenName: botOptions.botData.sendTokenName,
             amount: amount,
@@ -34,32 +78,18 @@ const requestTransferToken = async (tokenId, tokenName, amount, wallet) => {
                 Authorization: loginData.token,
             },
         });
-        console.log('requestTransferToken Success: ', test)
+        console.log('requestTransferToken Success: ', test.data)
         return true;
-    } catch (error){
-        console.log('====> request TransferToken Error: ', error.response.status)
-        refreshToken().then(response => {
-            console.log('==================>', response.data)
-            requestTransferToken(tokenId, tokenName, amount, wallet).then(responseTransfer => {
-                console.log('Request transfer after refresh', loginData.token)
-                return responseTransfer;
-            }).catch(error => {
-                console.log('transferToken after refresh Error: ', error.data);
-                return false;
-            });
-        }).catch(error =>{
-            console.log('refreshToken Error: ', error.data);
-            return false;
-        });
-
-
-        return error;
+    }
+    catch (error){
+        JSON.stringify(error)
+        throw error;
     }
 }
 
 export const mintNft = async (contractAddress, slot, amount, walletAddress) => {
     try {
-        let test = await axios.post(botOptions.apiUrl + 'tokens/items/nfmt/mint', {
+        let test = await http.post('tokens/items/nfmt/mint', {
             contractAddress: contractAddress,
             slot: slot,
             amount: amount,
@@ -73,76 +103,16 @@ export const mintNft = async (contractAddress, slot, amount, walletAddress) => {
         console.log('mintNft Success: ', test.data)
         return true;
     } catch (error){
-        console.log('====> request mintNft Error: ', error.response.status)
-        refreshToken().then(response => {
-            console.log('==================>', response.data)
-            mintNft(tokenId, tokenName, amount, wallet).then(responseTransfer => {
-                console.log('Request transfer after refresh', loginData.token)
-                return responseTransfer;
-            }).catch(error => {
-                console.log('transferToken after refresh Error: ', error.data);
-                return false;
-            });
-        }).catch(error =>{
-            console.log('refreshToken Error: ', error.data);
-            return false;
-        });
-
-
-        return error;
-    }
-}
-
-export const transferToken = async (tokenId, tokenName, amount, wallet) => {
-        requestTransferToken(tokenId, tokenName, amount, wallet).then(response => {
-            console.log('TRANSFER ===================', response)
-        }).catch(error => {
-            console.log('transferToken Error: ', error.response);
-
-        })
-}
-
-const refreshToken = async () => {
-    if (loginData.length <= 0) {
-        console.log('=> Bot not logged in')
-        return false;
-    }
-
-    try {
-        const request = await axios.post(botOptions.apiUrl + 'users/login/refresh', {}, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: loginData.refreshToken,
-            },
-        });
-        loginData.token = request.data.token;
-        loginData.refreshToken = request.data.refreshToken;
-        return true;
-    } catch (error) {
-        console.log('refreshToken Error: ', error);
-        return false;
+        JSON.stringify(error)
+        throw error;
     }
 }
 
 export const getMintItemData = async (walletAddress) => {
     try {
-        let itemData = await axios.get(botOptions.apiUrl + 'tokens/get/'+walletAddress);
+        let itemData = await http.get('tokens/get/'+walletAddress);
         return itemData.data.results;
     }catch (error){
         console.log('Error getMintItemData: ', error)
     }
-}
-
-const handlerRefreshToken = (amount, wallet) => {
-    console.log('=> Token is outdated, launch refreshToken')
-    refreshToken().then(response => {
-        console.log('==================>', response)
-        if (response) {
-            // return transferToken(amount, wallet);
-            return true;
-        }
-        return false;
-    }).catch(() => {
-        return false;
-    });
 }
