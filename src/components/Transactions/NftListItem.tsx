23e8df7254570d4aa8ad147c9e0480c7db98beed
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-09-05 09:52:09 +0300

    Adaptation for collections

--- src/components/Transactions/NftListItem.tsx
+++ src/components/Transactions/NftListItem.tsx
@@ -19,9 +19,17 @@ import {
 } from 'react-native-responsive-screen';
 import {imageMimetypes, videoMimetypes} from '../../constants/mimeTypes';
 import AntIcon from 'react-native-vector-icons/AntDesign';
-import {commonColors, textStyles} from '../../../docs/config';
+import {
+  coinImagePath,
+  commonColors,
+  defaultBotsList,
+  textStyles,
+} from '../../../docs/config';
 import FastImage from 'react-native-fast-image';
-import {NFMT_TYPES} from '../../stores/walletStore';
+import {NFMT_TRAITS, NFMT_TYPES} from '../../stores/walletStore';
+import {HStack, VStack} from 'native-base';
+import {createPrivateChat} from '../../helpers/chat/createPrivateChat';
+import {useStores} from '../../stores/context';
 
 interface NftListItemProps {
   assetUrl: string;
@@ -37,6 +45,20 @@ interface NftListItemProps {
   onAssetPress?: () => void;
 }
 
+const NfmtTag = ({tag}: {tag: string}) => {
+  return (
+    <HStack
+      style={{backgroundColor: NFMT_TRAITS[tag].color || 'green'}}
+      paddingX={2}
+      borderRadius={5}
+      marginX={1}
+      paddingY={0.5}
+      marginTop={0.5}>
+      <Text style={{color: 'white'}}>{tag}</Text>
+    </HStack>
+  );
+};
+
 export const NftListItem = (props: NftListItemProps) => {
   const {
     assetUrl,
@@ -51,6 +73,19 @@ export const NftListItem = (props: NftListItemProps) => {
     itemSelected,
     onAssetPress,
   } = props;
+  const {loginStore, apiStore, chatStore} = useStores();
+  const onGetCollectionPress = () => {
+    const bot = defaultBotsList.find(item => item.name === 'Merchant Bot');
+    const {} = createPrivateChat(
+      loginStore.initialData.walletAddress,
+      bot.name,
+      loginStore.initialData.firstName,
+      bot.name,
+      apiStore.xmppDomains.CONFERENCEDOMAIN,
+      chatStore.xmpp,
+    );
+  };
+
   return (
     <View
       onPress={onClick}
@@ -87,23 +122,46 @@ export const NftListItem = (props: NftListItemProps) => {
               </TouchableWithoutFeedback>
             )}
           </View>
-          <TouchableWithoutFeedback
-            onPress={onClick}
-            style={{width: wp('70%'), height: '100%'}}>
-            <View style={{width: wp('70%')}}>
+          <TouchableWithoutFeedback onPress={onClick} style={{height: '100%'}}>
+            <View>
               <Text style={styles.itemName}>{name}</Text>
             </View>
           </TouchableWithoutFeedback>
         </View>
-        {!item?.isCollection  && (
-          <TouchableWithoutFeedback onPress={onClick}>
-            <View style={styles.itemCount}>
+
+        <TouchableWithoutFeedback onPress={onClick}>
+          <View style={styles.itemCount}>
+            <VStack>
+              {item?.traits &&
+                !item.isCollection &&
+                item.traits.map((trait, i) => {
+                  return <NfmtTag tag={trait} key={item} />;
+                })}
+            </VStack>
+            <View>
               <Text style={{color: 'black'}}>
-                {assetsYouHave}/{totalAssets}
+                <Text
+                  style={{
+                    color: item.isCollection ? 'green' : 'black',
+                    fontFamily: item.isCollection
+                      ? textStyles.semiBoldFont
+                      : '',
+                  }}>
+                  {assetsYouHave}
+                </Text>
+                /{totalAssets}
               </Text>
+              {item.isCollection && (
+                <Text style={{color: 'black'}}>
+                  <Image source={coinImagePath} style={styles.tokenIconStyle} />
+                  {Math.min(...item.costs)} -{' '}
+                  <Image source={coinImagePath} style={styles.tokenIconStyle} />
+                  {Math.max(...item.costs)}
+                </Text>
+              )}
             </View>
-          </TouchableWithoutFeedback>
-        )}
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     </View>
   );
@@ -121,10 +179,10 @@ const styles = StyleSheet.create({
   justifyAround: {
     flexDirection: 'row',
     width: '100%',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
   },
   itemContainer: {
-    width: wp('100%'),
+    // width: wp('100%'),
 
     // backgroundColor: '#F4F5F8',
     flexDirection: 'row',
@@ -152,11 +210,16 @@ const styles = StyleSheet.create({
   },
   itemCount: {
     // backgroundColor: '#F4F5F8',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    paddingRight: 20,
+    justifyContent: 'flex-start',
+    paddingRight: 30,
   },
   nfmtStyle: {
     borderWidth: 1,
+  },
+  tokenIconStyle: {
+    height: hp('3%'),
+    width: hp('3%'),
   },
 });
