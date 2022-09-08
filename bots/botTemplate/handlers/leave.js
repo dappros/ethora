b8ed15b77bdfe70094a6b0be28a75c93fecb9f54
Anton Kolot <antvelkol@gmail.com>
2022-09-08 12:16:27 +0300

    Updated the logic of leave with new user steps

--- bots/botTemplate/handlers/leave.js
+++ bots/botTemplate/handlers/leave.js
@@ -1,16 +1,13 @@
-import {sendMessage, userData, userSteps} from "../actions.js";
+import {sendMessage, userSteps} from "../actions.js";
 import messages from "../config/messages.js";
 
 export const leaveHandler = (data) => {
     console.log('=> leaveHandler | Message received from ', data.receiver, data.message);
-    userSteps('setStep', data.receiverData.attrs.senderJID, 1);
-    userData('setData', data.userJID, [], 'itemData');
-    userData('setData', data.userJID, 0, 'sendCoins');
-    userData('setData', data.userJID, 0, 'itemDataIndex');
+    userSteps('setStep', data.userJID, 1);
 
     return sendMessage(
         data,
-        'You exited the merchant process',
+        messages.exampleBotMessage.leaveMessage,
         'message',
         false,
         0,
