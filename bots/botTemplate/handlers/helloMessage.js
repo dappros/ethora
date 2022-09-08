af02d357e87212f346503df5489e71195a40e540
Anton Kolot <antvelkol@gmail.com>
2022-09-08 12:13:28 +0300

    Updated the function of sending messages for presence

--- bots/botTemplate/handlers/helloMessage.js
+++ bots/botTemplate/handlers/helloMessage.js
@@ -1,8 +1,8 @@
 import {sendMessage} from "../actions.js";
 import messages from "../config/messages.js";
 
-export const helloMessageHandler = (data) => {
-    console.log('=> helloMessageHandler || Message received from ', data.userJID, data.message);
+export const presenceMessageHandler = (data) => {
+    console.log('=> presenceMessageHandler || Message received from ', data.userJID, data.message);
     return sendMessage(
         data,
         messages.exampleBotMessage.helloPresence,
