cbdf96f15b36abdc56418aad2b474e3c58dd23ae
Anton Kolot <antvelkol@gmail.com>
2022-09-08 16:34:59 +0300

    Added standard message processing conditions

--- bots/botTemplate/router.js
+++ bots/botTemplate/router.js
@@ -1,21 +1,26 @@
-import {messageCheck, sendMessage, userData, userSteps} from "./actions.js";
+import {messageCheck, sendMessage, userSteps} from "./actions.js";
 import {leaveHandler} from "./handlers/leave.js";
 import messages from "./config/messages.js";
 import botOptions from "./config/config.js";
+import {helpHandler} from "./handlers/help.js";
+
 const router = (handlerData) => {
+    handlerData.userStep = userSteps('getStep', handlerData.userJID);
 
     if (messageCheck(handlerData.message, 'close') || messageCheck(handlerData.message, 'leave')) {
         return leaveHandler(handlerData);
     }
-    // if (messageCheck(handlerData.message, 'test')) {
-    //
-    //     // console.log('RUN TEST GET USER ROOMS => => => ', connectData.botName+'@'+connectData.botAddress)
-    //     // return getBotRoomsStanza(xmpp, connectData);
-    // }
 
+    if (messageCheck(handlerData.message, 'bot help') ||
+        messageCheck(handlerData.message, 'bots help') ||
+        messageCheck(handlerData.message, 'bot info') ||
+        messageCheck(handlerData.message, 'bots info'))
+    {
+        return helpHandler(handlerData);
+    }
 
     if (handlerData.receiverData.isSystemMessage && handlerData.receiverData.tokenAmount > 0) {
-        if (messageCheck(message, botOptions.botData.firstName + ' ' + botOptions.botData.lastName)) {
+        if (messageCheck(handlerData.message, botOptions.botData.firstName + ' ' + botOptions.botData.lastName)) {
             return sendMessage(
                 handlerData,
                 messages.bot.tnxForTransaction,
@@ -25,7 +30,5 @@ const router = (handlerData) => {
             );
         }
     }
-    // receiverData.attrs.notDisplayedValue
-
 }
 export {router};
