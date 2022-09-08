18b0b53eb9e920809dea06a087a43014bd789cd4
Anton Kolot <antvelkol@gmail.com>
2022-09-08 16:34:07 +0300

    Updated variables in the log leaveHandler

--- bots/botTemplate/handlers/leave.js
+++ bots/botTemplate/handlers/leave.js
@@ -2,7 +2,7 @@ import {sendMessage, userSteps} from "../actions.js";
 import messages from "../config/messages.js";
 
 export const leaveHandler = (data) => {
-    console.log('=> leaveHandler | Message received from ', data.receiver, data.message);
+    console.log('=> leaveHandler | Message received from ', data.userJID, data.message);
     userSteps('setStep', data.userJID, 1);
 
     return sendMessage(
