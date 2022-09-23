865a3a87600602314b31b4c23d3984cddcdef793
Anton Kolot <antvelkol@gmail.com>
2022-09-23 16:22:24 +0300

    add new handlers to show data

--- bots/autoResponder/router.js
+++ bots/autoResponder/router.js
@@ -1,14 +1,48 @@
-import {messageCheck, sendMessage, userSteps} from "./actions.js";
+import {getUserRooms, messageCheck, sendMessage, userSteps} from "./actions.js";
 import {leaveHandler} from "./handlers/leave.js";
 import messages from "./config/messages.js";
 import botOptions from "./config/config.js";
 import {helpHandler} from "./handlers/help.js";
+import {testHandler} from "./handlers/test.js";
 
 const router = (handlerData) => {
     handlerData.userStep = userSteps('getStep', handlerData.userJID);
 
     if (messageCheck(handlerData.message, 'close') || messageCheck(handlerData.message, 'leave')) {
         return leaveHandler(handlerData);
+    }
+
+    if (messageCheck(handlerData.message, 'test')) {
+        return testHandler(handlerData);
+    }
+
+    if (messageCheck(handlerData.message, 'start')) {
+        return getUserRooms(handlerData);
+    }
+
+    if (handlerData.receiverData.notDisplayedValue === 'chat') {
+        return sendMessage(
+            handlerData,
+            messages.youHaveAChat,
+            'message',
+            false,
+            0,
+        );
+    }
+
+    if (handlerData.receiverData.notDisplayedValue === 'send_information') {
+        return sendMessage(
+            handlerData,
+            messages.informationAboutCompany,
+            'message',
+            false,
+            0,
+            [{
+                name: 'Chat',
+                value: 'Chat',
+                notDisplayedValue: "chat"
+            }]
+        );
     }
 
     if (messageCheck(handlerData.message, 'bot help') ||
