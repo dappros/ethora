1ddc6ec2e40b8974bb1f53ba89ebbf6b9c9aeea5
Anton Kolot <antvelkol@gmail.com>
2022-09-23 16:21:40 +0300

    update presence handler logic

--- bots/autoResponder/handlers/helloMessage.js
+++ bots/autoResponder/handlers/helloMessage.js
@@ -1,13 +1,6 @@
-import {sendMessage} from "../actions.js";
-import messages from "../config/messages.js";
+import {getUserRooms} from "../actions.js";
 
 export const presenceMessageHandler = (data) => {
     console.log('=> presenceMessageHandler || Message received from ', data.userJID, data.message);
-    return sendMessage(
-        data,
-        messages.exampleBotMessage.helloPresence,
-        'message',
-        false,
-        0,
-    );
+    getUserRooms(data);
 }
