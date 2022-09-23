dec65d0412faec58806fabb240e1283c5b5234dd
Anton Kolot <antvelkol@gmail.com>
2022-09-23 16:23:47 +0300

    change text in rest handler

--- bots/autoResponder/handlers/test.js
+++ bots/autoResponder/handlers/test.js
@@ -1,11 +1,11 @@
-import {sendMessage} from "../actions.js";
+import {getUserRooms, sendMessage} from "../actions.js";
 import messages from "../config/messages.js";
 
 export const testHandler = (data) => {
     console.log('=> Message received from ', data.userJID, data.message);
     sendMessage(
         data,
-        messages.testMessage,
+        'get rooms',
         'message',
         false,
         0,
