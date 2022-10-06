81625d5b6b62c6025cb1f7ceedb7eb7d06904209
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-10-06 14:57:59 +0300

    Types changing

--- client-web/src/pages/Profile/MyProfile.tsx
+++ client-web/src/pages/Profile/MyProfile.tsx
@@ -1,29 +1,34 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@mui/material/Container";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
-import {TProfile, TTransactions} from './types'
+import {
+  ExplorerRespose,
+  ITransaction,
+  TProfile,
+  TTransactions,
+} from "./types";
 import UserCard from "./UserCard";
 import { getPublicProfile, getTransactions, getBalance } from "../../http";
-import TransactionsTable from './TransactionsTable'
-import OtherItems from './OtherItems'
-import {useState}  from '../../store'
-import ItemsTable from './ItemsTable'
-
+import TransactionsTable from "./TransactionsTable";
+import OtherItems from "./OtherItems";
+import { useState as useStoreState } from "../../store";
+import ItemsTable from "./ItemsTable";
 
 type TBalance = {
-  balance: string,
-  tokenName: string
-}
+  balance: string;
+  tokenName: string;
+};
 
 export default function MyProfile() {
-  const [loading, setLoading] = React.useState(true);
-  const [profile, setProfile] = React.useState<TProfile>();
-  const [transactions, setTransactions] = React.useState<TTransactions>();
-  const [balances, setBalances] = React.useState<TBalance>()
-  const user = useState((store) => store.user)
+  const [loading, setLoading] = useState(true);
+  const [profile, setProfile] = useState<TProfile>();
+  const [transactions, setTransactions] =
+    useState<ExplorerRespose<ITransaction[]>>();
+  const [balances, setBalances] = useState<TBalance>();
+  const user = useStoreState((store) => store.user);
 
-  React.useEffect(() => {
+  useEffect(() => {
     setLoading(true);
     getPublicProfile(user.walletAddress)
       .then((result) => {
@@ -31,20 +36,18 @@ export default function MyProfile() {
       })
       .finally(() => setLoading(false));
 
-      getTransactions(user.walletAddress)
-        .then((result) => {
-          setTransactions(result.data)
-          console.log('balance ', result.data)
-        })
+    getTransactions(user.walletAddress).then((result) => {
+      setTransactions(result.data);
+      console.log("balance ", result.data);
+    });
 
-      getBalance(user.walletAddress)
-        .then((result) => {
-          setBalances(result.data)
-        })
+    getBalance(user.walletAddress).then((result) => {
+      setBalances(result.data);
+    });
   }, []);
 
   return (
-    <Container maxWidth="xl" style={{ height: "calc(100vh - 80px)"}}>
+    <Container maxWidth="xl" style={{ height: "calc(100vh - 80px)" }}>
       {loading ? (
         <Box
           style={{
@@ -61,11 +64,15 @@ export default function MyProfile() {
           <CircularProgress />
         </Box>
       ) : null}
-      <Box style={{display: 'flex'}}>
+      <Box style={{ display: "flex" }}>
         {profile ? <UserCard profile={profile}></UserCard> : null}
       </Box>
       <ItemsTable></ItemsTable>
-      {transactions ? <TransactionsTable transactions={transactions}></TransactionsTable> : null}
+      {transactions ? (
+        <TransactionsTable
+          transactions={transactions.items}
+        ></TransactionsTable>
+      ) : null}
     </Container>
   );
 }
