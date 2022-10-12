4e07bd9af7c85fbf3fbeff2fdd3e72a666a023de
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-10-12 09:36:16 +0300

    helper

--- client-web/src/utils/index.ts
+++ client-web/src/utils/index.ts
@@ -1,8 +1,13 @@
-import * as React from 'react'
-import {useLocation} from 'react-router-dom';
+import * as React from "react";
+import { useLocation } from "react-router-dom";
 
 export function useQuery() {
   const { search } = useLocation();
 
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
+export const truncateString = (input: string, textLength: number) => {
+  return input.length > textLength
+    ? `${input.substring(0, textLength)}...`
+    : input;
+};
