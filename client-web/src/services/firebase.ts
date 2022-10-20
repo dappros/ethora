1689a108bcc9e69ec35e228b4de3d13bf7ee958b
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-10-20 15:39:06 +0300

    moved to typescript

---
+++ client-web/src/services/firebase.ts
@@ -0,0 +1,38 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  User,
+} from "firebase/auth";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID,
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope("https://www.googleapis.com/auth/userinfo.email");
+
+type IUser = User & { accessToken: string };
+
+export const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user as IUser;
+    const idToken = await auth?.currentUser?.getIdToken();
+    return {
+      user,
+      idToken,
+    };
+  } catch (err) {
+    console.error(err);
+    return {};
+  }
+};
