fb0245f275969d5a6dabd223bda05a232ab8457d
ibnsan <antvelkol@gmail.com>
2022-11-03 16:27:31 +0100

    Update counter only when incoming messages arrive

--- client-web/src/xmpp.ts
+++ client-web/src/xmpp.ts
@@ -6,6 +6,7 @@ import { TMessageHistory, useStoreState } from "./store";
 let lastMsgId: string = "";
 let temporaryMessages: TMessageHistory[] = [];
 let isGettingMessages: boolean = false;
+let isGettingFirstMessages: boolean = false;
 let lastRomJIDLoading: string = "";
 
 export function walletToUsername(str: string) {
@@ -103,8 +104,8 @@ const onMessageHistory = async (stanza: Element) => {
       useStoreState.getState().setNewMessageHistory(msg);
       useStoreState.getState().sortMessageHistory();
     }
-
-    if (stanza.attrs.to.split("@")[0] !== data.attrs.senderJID) {
+    if (stanza.attrs.to.split("@")[0] !== data.attrs.senderJID.split("@")[0] &&
+        !isGettingFirstMessages) {
       useStoreState.getState().updateCounterChatRoom(data.attrs.roomJid);
     }
   }
@@ -123,11 +124,13 @@ const onLastMessageArchive = (stanza: Element, xmpp: any) => {
       isGettingMessages = false;
       useStoreState.getState().setLoaderArchive(false);
       temporaryMessages = [];
+      isGettingFirstMessages = false;
     }
 
     if (lastRomJIDLoading && lastRomJIDLoading === stanza.attrs.from) {
       useStoreState.getState().setLoaderArchive(false);
       lastRomJIDLoading = "";
+      isGettingFirstMessages = false;
     }
   }
 };
@@ -146,6 +149,7 @@ const onGetLastMessageArchive = (stanza: Element, xmpp: any) => {
 const connectToUserRooms = (stanza: Element, xmpp: any) => {
   if (stanza.attrs.id === "getUserRooms") {
     if (stanza.getChild("query")?.children) {
+      isGettingFirstMessages = true;
       useStoreState.getState().clearUserChatRooms();
       useStoreState.getState().setLoaderArchive(true);
       let roomJID: string = "";
@@ -260,7 +264,6 @@ class XmppClass {
     this.client.on("stanza", (stanza) => connectToUserRooms(stanza, this));
     this.client.on("stanza", (stanza) => onLastMessageArchive(stanza, this));
     this.client.on("stanza", (stanza) => onComposing(stanza));
-    // this.client.on("stanza", (stanza) => console.log(stanza));
 
     this.client.on("offline", () => console.log("offline"));
     this.client.on("error", (error) => {
@@ -437,6 +440,7 @@ class XmppClass {
       return;
     }
     isGettingMessages = true;
+    isGettingFirstMessages = true;
     useStoreState.getState().setLoaderArchive(true);
     const message = xml(
       "iq",
