62ebdfd750d7305913d21b0ded860f683b0b0469
Boris <transkarpation@gmail.com>
2022-11-07 15:57:03 +0200

    chanks

--- client-web/src/pages/Routes.tsx
+++ client-web/src/pages/Routes.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect } from "react";
 import { Route, Switch } from "react-router";
-import Chat from "./Chat";
 import CreateApp from "./CreateApp";
-import { BlockDetails } from "./Explorer/BlockDetails";
-import { Blocks } from "./Explorer/Blocks";
-import ChatInRoom from "./ChatInRoom";
-import { Explorer } from "./Explorer/Explorer";
 import { TransactionAddressDetails } from "./Explorer/TransactionAddressDetails";
 import { TransactionDetails } from "./Explorer/TransactionDetails";
-import Owner from "./Owner";
-import Profile from "./Profile";
-import { Signon } from "./Signon";
+import { Blocks } from "./Explorer/Blocks";
 import { useStoreState } from "../store";
 import { getMyAcl } from "../http";
+const ChatInRoom = React.lazy(() => import("./ChatInRoom"));
+const Profile = React.lazy(() => import("./Profile"));
+const Signon = React.lazy(() => import("./Signon"));
+const Owner = React.lazy(() => import("./Owner"));
+const BlockDetails = React.lazy(() => import("./Explorer/BlockDetails"));
+const Explorer = React.lazy(() => import("./Explorer/Explorer"));
 
 export const Routes = () => {
   const userId = useStoreState((state) => state.user._id);
@@ -86,42 +85,41 @@ export const Routes = () => {
   }, [userId]);
 
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Signon />
-      </Route>
-      <Route path="/chat">
-        <Chat />
-      </Route>
-      <Route path="/chat-in-room">
-        <ChatInRoom />
-      </Route>
-      <Route path="/owner">
-        <Owner />
-      </Route>
-      <Route path="/owner/create-app">
-        <CreateApp />
-      </Route>
-      <Route path="/profile/:wallet">
-        <Profile />
-      </Route>
-      <Route path={"/explorer"} component={Explorer} exact />
-      <Route
-        path={"/explorer/block/:blockNumber"}
-        component={BlockDetails}
-        exact
-      />
-      <Route path={"/explorer/blocks/"} component={Blocks} exact />
-      <Route
-        path={"/explorer/transactions/:txId"}
-        component={TransactionDetails}
-        exact
-      />
-      <Route
-        path={["/explorer/address/:address", "/explorer/app/:address"]}
-        component={TransactionAddressDetails}
-        exact
-      />
-    </Switch>
+    <React.Suspense fallback={<p>loading</p>}>
+      <Switch>
+        <Route path="/" exact>
+          <Signon />
+        </Route>
+        <Route path="/chat-in-room">
+          <ChatInRoom />
+        </Route>
+        <Route path="/owner">
+          <Owner />
+        </Route>
+        <Route path="/owner/create-app">
+          <CreateApp />
+        </Route>
+        <Route path="/profile/:wallet">
+          <Profile />
+        </Route>
+        <Route path={"/explorer"} component={Explorer} exact />
+        <Route
+          path={"/explorer/block/:blockNumber"}
+          component={BlockDetails}
+          exact
+        />
+        <Route path={"/explorer/blocks/"} component={Blocks} exact />
+        <Route
+          path={"/explorer/transactions/:txId"}
+          component={TransactionDetails}
+          exact
+        />
+        <Route
+          path={["/explorer/address/:address", "/explorer/app/:address"]}
+          component={TransactionAddressDetails}
+          exact
+        />
+      </Switch>
+    </React.Suspense>
   );
 };
