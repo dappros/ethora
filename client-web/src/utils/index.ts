b9fd26be9829fcd6682863c61b7cfa402a9cfd2d
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-11-09 13:42:33 +0200

    utils

--- client-web/src/utils/index.ts
+++ client-web/src/utils/index.ts
@@ -34,3 +34,41 @@ export function sendBrowserNotification(
   });
   notification.onclick = onNotificationClick;
 }
+
+export const produceNfmtItems = (array = []) => {
+  const result = [];
+  const rareTotal = 20;
+  const uniqueTotal = 1;
+
+  for (const item of array) {
+    if (item.tokenType === 'NFMT') {
+      for (let i = 0; i < item.balances.length; i++) {
+        const tokenBalance = item.balances[i];
+        const tokenType = +item.contractTokenIds[i];
+        const total = item.maxSupplies.find((supply, i) => tokenType === i + 1);
+        const traits = item.traits.map(trait =>
+          trait.find((el, i) => tokenType === i + 1),
+        );
+        total < rareTotal && traits.push('Rare');
+        total === uniqueTotal && traits.push('Unique!');
+        const resItem = {
+          ...item,
+          balance: tokenBalance,
+          nfmtType: tokenType,
+          total: total,
+          traits,
+        };
+        +tokenBalance > 0 && result.push(resItem);
+      }
+    }
+  }
+  return result;
+};
+
+export const filterNftBalances = (item) => {
+  return (
+    (item.tokenType === "NFT" || item.tokenType === "NFMT") &&
+    (item.balance > 0 ||
+      (item.balances.length && item.balances.some((item) => +item > 0)))
+  );
+};
