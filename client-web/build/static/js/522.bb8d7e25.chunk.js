83aa473a1b1b3f5a3cf68968e4eee17a0f3db229
Boris <transkarpation@gmail.com>
2022-11-10 14:10:36 +0200

    build

---
+++ client-web/build/static/js/522.bb8d7e25.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb_app=self.webpackChunkweb_app||[]).push([[522],{79620:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});t(72791);var c=t(57020),p=t(65931),r=t(50228),s=t(80184);function u(){return(0,s.jsx)(c.Z,{maxWidth:"xl",style:{height:"calc(100vh - 68px)"},children:(0,s.jsx)(r.Z,{style:{marginTop:"20px"},children:(0,s.jsx)(p.Z,{})})})}}}]);
+//# sourceMappingURL=522.bb8d7e25.chunk.js.map
