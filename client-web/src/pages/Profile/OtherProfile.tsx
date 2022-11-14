ec4f4366e40e325c92ce5a98406986bdbf1e58f1
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-11-14 12:36:24 +0200

    added files fetching

--- client-web/src/pages/Profile/OtherProfile.tsx
+++ client-web/src/pages/Profile/OtherProfile.tsx
@@ -29,44 +29,33 @@ export function OtherProfile(props: TProps) {
     useState<ExplorerRespose<ITransaction[]>>();
   const [balances, setBalances] = useState<TBalance[]>([]);
   const [documents, setDocuments] = useState<IDocument[]>([]);
-  const getDocuments = async (walletAddress: string) => {
-    try {
-      const docs = await http.httpWithAuth().get(`/docs/${walletAddress}`);
-
-      const documents = docs.data.results;
-      const mappedDocuments = [];
-      for (const item of documents) {
-        try {
-          const { data: file } = await http
-            .httpWithAuth()
-            .get<http.IDocument[]>("/files/" + item.files[0]);
-          item.file = file;
-          mappedDocuments.push(item);
-        } catch (error) {
-          console.log(item.files[0], "sdjfkls");
-        }
+  const getDocuments = async (documents: IDocument[]) => {
+    const mappedDocuments = [];
+    for (const item of documents) {
+      try {
+        const { data: file } = await http
+          .httpWithAuth()
+          .get<http.IFile>("/files/" + item.files[0]);
+        item.file = file;
+        mappedDocuments.push(item);
+      } catch (error) {
+        console.log( item.files[0],"sdjfkls");
       }
-      setDocuments(mappedDocuments);
-    } catch (error) {
-      console.log(error, "404");
     }
+    setDocuments(mappedDocuments);
   };
   useEffect(() => {
     setLoading(true);
     getPublicProfile(props.walletAddress).then((result) => {
-      console.log("getPublicProfile ", result.data);
+      console.log("getPublicProfile ", result.data.documents);
       setProfile(result.data);
+      setBalances(result.data.balances);
+      getDocuments(result.data.documents);
     });
     // .finally(() => setLoading(false));
     setLoading(false);
     getTransactions(props.walletAddress).then((result) => {
       setTransactions(result.data);
-    });
-    getDocuments(props.walletAddress);
-    getBalance(props.walletAddress).then((result) => {
-      console.log("balance ", result.data);
-
-      setBalances(result.data.balance);
     });
   }, []);
 
