348f985a9dbe2b192a654e5cba59ae657fc5a7ff
Borys <transkarpation@gmail.com>
2022-11-18 13:41:35 +0200

    remove check for user

--- client-web/src/pages/ChatInRoom/index.tsx
+++ client-web/src/pages/ChatInRoom/index.tsx
@@ -6,15 +6,8 @@ import { useHistory } from "react-router-dom";
 import { ChatInRoom } from "./Chat";
 
 export default function Chat() {
-  const user = useStoreState((state) => state.user);
   const messages = useStoreState((state) => state.messages);
-  const history = useHistory();
 
-  useEffect(() => {
-    if (!user.firstName) {
-      history.push("/");
-    }
-  }, [user.firstName, history]);
   return (
     <Container maxWidth="xl" style={{ height: "calc(100vh - 68px)" }}>
       <Box>Chat</Box>
