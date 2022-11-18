3794ae7b4939e9dc9496058c71e956deb17b2207
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2022-11-18 11:57:24 +0200

    #218 added padding, removed blue line

--- client-reactnative/src/Screens/ProfileScreen.tsx
+++ client-reactnative/src/Screens/ProfileScreen.tsx
@@ -447,7 +447,7 @@ export const ProfileScreen = observer((props: any) => {
             )}
           </HStack>
 
-          <View style={{marginBottom: hp('24%')}}>
+          <View style={{marginBottom: hp('34%'), backgroundColor: 'white'}}>
             {activeAssetTab === 1 &&
               walletStore.nftItems.length === 0 &&
               walletStore.documents.length === 0 && (
