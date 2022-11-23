7ecc820c31287eef0620fee5333d37f69264a368
Anton Kolot <antvelkol@gmail.com>
2022-11-23 16:52:25 +0200

    remove "chat 0"

--- client-web/src/pages/ChatInRoom/index.tsx
+++ client-web/src/pages/ChatInRoom/index.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import { useStoreState } from "../../store";
 import { ChatInRoom } from "./Chat";
 
 export default function Chat() {
-  const messages = useStoreState((state) => state.messages);
-
   return (
     <Container maxWidth="xl" style={{ height: "calc(100vh - 68px)" }}>
-      <Box>Chat</Box>
-      <Box>{messages.length}</Box>
       <ChatInRoom />
     </Container>
   );
