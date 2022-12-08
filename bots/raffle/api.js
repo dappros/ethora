7a098e109838d6acb9117721a22eb68721f6aefd
Anton Kolot <antvelkol@gmail.com>
2022-12-08 12:04:57 +0200

    added function for token transfer

--- bots/raffle/api.js
+++ bots/raffle/api.js
@@ -86,6 +86,27 @@ export const transferToken = async (tokenId, tokenName, amount, wallet) => {
     }
 }
 
+export const transferItem = async (nftId, receiverWallet, amount) => {
+    try {
+        let request = await http.post('tokens/transfer/items', {
+            nftId,
+            receiverWallet,
+            amount
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: loginData.token,
+            },
+        });
+        console.log('requestTransferItem Success: ', request.data)
+        return true;
+    } catch (error) {
+        console.log(error)
+        JSON.stringify(error)
+        throw error;
+    }
+}
+
 export const mintNft = async (contractAddress, slot, amount, walletAddress) => {
     try {
         let test = await http.post('tokens/items/nfmt/mint', {
