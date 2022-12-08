449256acc2d784f4ca3f74d965fc7632e05eb20c
Anton Kolot <antvelkol@gmail.com>
2022-12-08 16:25:53 +0200

    Update router and added work with coins

--- bots/raffle/router.js
+++ bots/raffle/router.js
@@ -1,14 +1,15 @@
-import {messageCheck, sendMessage, userSteps} from "./actions.js";
+import {messageCheck, userSteps} from "./actions.js";
 import {leaveHandler} from "./handlers/leave.js";
-import messages from "./config/messages.js";
-import botOptions from "./config/config.js";
 import {helpHandler} from "./handlers/help.js";
-import { botInitiate } from "./handlers/botInitiate.js";
-import {transferItemHandler} from "./handlers/transferItemHandler.js";
-import {getBalanceHandler} from "./handlers/getBalanceHandler.js";
+import {botInitiate} from "./handlers/botInitiate.js";
+import {gettingCoinsHandler} from "./handlers/coins/gettingCoinsHandler.js";
 
 const router = (handlerData) => {
     handlerData.userStep = userSteps('getStep', handlerData.userJID);
+
+    if (handlerData.receiverData.isSystemMessage && handlerData.receiverData.tokenAmount > 0) {
+        return gettingCoinsHandler(handlerData);
+    }
 
     if (messageCheck(handlerData.message, 'close') || messageCheck(handlerData.message, 'leave')) {
         return leaveHandler(handlerData);
@@ -17,35 +18,20 @@ const router = (handlerData) => {
     if (messageCheck(handlerData.message, 'bot help') ||
         messageCheck(handlerData.message, 'bots help') ||
         messageCheck(handlerData.message, 'bot info') ||
-        messageCheck(handlerData.message, 'bots info'))
-    {
+        messageCheck(handlerData.message, 'bots info')) {
         return helpHandler(handlerData);
     }
 
-        if (messageCheck(handlerData.message, 'Raffle bot'))
-            {
-                return botInitiate(handlerData);
-            }
+    if (messageCheck(handlerData.message, 'Raffle bot') ||
+        messageCheck(handlerData.message, 'raffle')
+    ) {
+        return botInitiate(handlerData);
     }
-    
 
-    if (handlerData.receiverData.isSystemMessage && handlerData.receiverData.tokenAmount > 0) {
-        if (messageCheck(handlerData.message, botOptions.botData.firstName + ' ' + botOptions.botData.lastName)) {
-            if(handlerData.receiverData.tokenAmount >= 7){
-                return sendMessage(
-                    handlerData,
-                    messages.raffleBotReplies.durationToParticipate,
-                    'message',
-                    false,
-                    0,
-                    messages.menu.mainMenu
-                );
-            }
-
-            if(handlerData.receiverData.tokenAmount < 7){
-
-            }
-        }
+    if(handlerData.userStep === 2){
+    //    next step...
     }
+
+}
 
 export {router};
