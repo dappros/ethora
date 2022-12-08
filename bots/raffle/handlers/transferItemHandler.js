1d797b335dc0fe00e49765386cbc7f78670b04ee
Anton Kolot <antvelkol@gmail.com>
2022-12-08 13:16:28 +0200

    Updated transfer logic

--- bots/raffle/handlers/transferItemHandler.js
+++ bots/raffle/handlers/transferItemHandler.js
@@ -1,17 +1,41 @@
-import {sendMessage} from "../actions.js";
-import {transferItem} from "../api.js";
+import {logCurrentHandler, sendMessage} from "../actions.js";
+import {getBalance, transferItem} from "../api.js";
 import botOptions from "../config/config.js";
+import {getRandomNFT} from "./helpers/getRandomNFT.js";
+import {requestError} from "./errors.js";
 
 export const transferItemHandler = (data) => {
-    console.log('=> Message received from ', data.userJID, data.message);
-    transferItem('6391ad07d1fbc4394f61d905', data.receiverData.senderWalletAddress, 1).then(() => {
-        sendMessage(
-            data,
-            botOptions.botData.firstName + ' ' + botOptions.botData.lastName + ' -> ' + 1 + ' ' + 'JO' + ' -> ' + data.receiverData.senderFirstName + " " + data.receiverData.senderLastName,
-            'message',
-            true,
-            0,
-        );
+    logCurrentHandler('transferItemHandler', data.userJID, data.message);
+    getBalance().then(result => {
+        const randomItem = getRandomNFT(result)[0];
+        if (randomItem) {
+            transferItem(randomItem.nftId, data.receiverData.senderWalletAddress, 1).then(() => {
+                sendMessage(
+                    data,
+                    generateSystemMessage(botOptions, data, randomItem.tokenName, 1),
+                    'message',
+                    true,
+                    0,
+                );
+            }).catch(error => {
+                requestError(data, 'getBalance', error)
+            })
+        } else {
+            sendMessage(
+                data,
+                "Warning: Items not found",
+                'message',
+                false,
+                0,
+            );
+        }
+    }).catch(error => {
+        requestError(data, 'getBalance', error)
     })
+}
 
+const generateSystemMessage = (botOptions, data, itemName, amount) => {
+    if (botOptions && data) {
+        return botOptions.botData.firstName + ' ' + botOptions.botData.lastName + ' -> ' + amount + ' ' + itemName + ' -> ' + data.receiverData.senderFirstName + " " + data.receiverData.senderLastName;
+    }
 }
