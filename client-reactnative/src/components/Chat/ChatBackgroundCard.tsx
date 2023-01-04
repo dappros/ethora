44daf7a8e10bbabee2b931c89dc9e3501cf4a5d3
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2023-01-04 19:05:31 +0200

    formatting

--- client-reactnative/src/components/Chat/ChatBackgroundCard.tsx
+++ client-reactnative/src/components/Chat/ChatBackgroundCard.tsx
@@ -1,56 +1,54 @@
-import { Box, Image, Pressable, Text } from 'native-base';
+import {Box, Image, Pressable, Text} from 'native-base';
 import * as React from 'react';
 import {
-    heightPercentageToDP as hp,
-    widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
-import { commonColors } from '../../../docs/config';
+import {commonColors} from '../../../docs/config';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import FastImage from 'react-native-fast-image';
 
 interface ChatBackgroundCardProps {
-    value?:string
-    alt?:string
-    isSelected?:boolean
-    onSelect:any
-    index:number
+  value?: string;
+  alt?: string;
+  isSelected?: boolean;
+  onSelect: any;
+  index: number;
 }
 
 const ChatBackgroundCard = (props: ChatBackgroundCardProps) => {
   return (
     <Pressable
-    justifyContent={"center"}
-    alignItems={"center"}
-    onPress={()=>props.onSelect(props.index)}
-    >
-        <Box
-        borderWidth={props.isSelected?2:0}
-        borderColor={props.isSelected?commonColors.primaryDarkColor:null}
-        opacity={props.isSelected?20:100}
+      justifyContent={'center'}
+      alignItems={'center'}
+      onPress={() => props.onSelect(props.index)}>
+      <Box
+        borderWidth={props.isSelected ? 2 : 0}
+        borderColor={props.isSelected ? commonColors.primaryDarkColor : null}
+        opacity={props.isSelected ? 20 : 100}
         zIndex={-1}
         h={hp('40%')}
         w={hp('20%')}
         borderRadius={5}
         justifyContent="center"
-        alignItems={"center"}
-        >
-            <FastImage
-            style={{
-              height:hp('40%'),
-              width:hp('20%'),
-              borderRadius:5,
-              
-            }}
-            source={{uri:props.value}}/>
-        </Box>
-        {props.isSelected?
+        alignItems={'center'}>
+        <FastImage
+          style={{
+            height: hp('40%'),
+            width: hp('20%'),
+            borderRadius: 5,
+          }}
+          source={{uri: props.value}}
+        />
+      </Box>
+      {props.isSelected ? (
         <AntDesign
-        color={"black"}
-        name="checkcircle"
-        size={hp('3%')}
-        style={{position:'absolute', zIndex:1}}/>:
-        null
-        }
+          color={'black'}
+          name="checkcircle"
+          size={hp('3%')}
+          style={{position: 'absolute', zIndex: 1}}
+        />
+      ) : null}
     </Pressable>
   );
 };
