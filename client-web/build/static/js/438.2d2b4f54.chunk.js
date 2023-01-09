9aa0a3d65ac2cde90f6646a5d8846fa4b321ae98
Михайло Могилюк <mykhailo@Mihajlos-MacBook-Air.local>
2023-01-09 16:58:09 +0200

    build without sourcemaps

---
+++ client-web/build/static/js/438.2d2b4f54.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkweb_app=self.webpackChunkweb_app||[]).push([[438],{39709:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});t(47313);var c=t(10679),p=t(99165),r=t(40229),s=t(46417);function u(){return(0,s.jsx)(c.Z,{maxWidth:"xl",style:{height:"calc(100vh - 68px)"},children:(0,s.jsx)(r.Z,{style:{marginTop:"20px"},children:(0,s.jsx)(p.Z,{})})})}}}]);
