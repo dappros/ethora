e51782650ff1785f17f89c49945d3d2495617ba7
Mykhailo Mohyliuk <mykhailo@Mihajlos-MacBook-Air.local>
2023-01-18 11:37:31 +0200

    build

---
+++ client-web/build/static/js/86.80e5bd7c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweb_app=self.webpackChunkweb_app||[]).push([[86],{79620:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});t(72791);var c=t(20803),p=t(27669),r=t(50228),s=t(80184);function u(){return(0,s.jsx)(c.Z,{maxWidth:"xl",style:{height:"calc(100vh - 68px)"},children:(0,s.jsx)(r.Z,{style:{marginTop:"20px"},children:(0,s.jsx)(p.Z,{})})})}}}]);
+//# sourceMappingURL=86.80e5bd7c.chunk.js.map
