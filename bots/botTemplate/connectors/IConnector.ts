45709030ef60f6fc6dc15f95ea389417c7c92a40
Anton Kolot <antvelkol@gmail.com>
2023-02-03 11:52:20 +0200

    Added connector interface

---
+++ bots/botTemplate/connectors/IConnector.ts
@@ -0,0 +1,16 @@
+import { IUser } from '../core/IUser';
+import { IMessage } from '../core/IMessage';
+
+export enum ConnectorEvent {
+    receiveMessage = 'receiveMessage'
+}
+
+export interface IConnector {
+    getConnectorName(): string;
+    getUniqueSessionKey(rawData?: {}): string;
+    getUser(): Promise<IUser>;
+    send(message: IMessage, user: IUser, options: any): Promise<void>;
+    listen(): any;
+    on(event: ConnectorEvent, handler: () => any): void;
+    emit(event: ConnectorEvent, data: any): void;
+}
