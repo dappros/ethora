4194f08adb62eac3f4a0f80979b76ed775dd431a
Anton Kolot <antvelkol@gmail.com>
2023-02-13 12:21:45 +0200

    Updated connector interface

--- bots/botTemplate/src/connectors/IConnector.ts
+++ bots/botTemplate/src/connectors/IConnector.ts
@@ -1,16 +1,20 @@
 import { IUser } from '../core/IUser';
 import { IMessage } from '../core/IMessage';
+import {IAuthorization} from "../api/IAuthorization";
 
 export enum ConnectorEvent {
     receiveMessage = 'receiveMessage'
 }
 
 export interface IConnector {
-    getConnectorName(): string;
-    getUniqueSessionKey(rawData?: {}): string;
-    getUser(): Promise<IUser>;
+    username: string;
+    password: string;
+    stanza: any;
+    botAuthData: IAuthorization;
+    getUniqueSessionKey(): string;
+    getUser(): IUser;
     send(message: IMessage, user: IUser, options: any): Promise<void>;
-    // listen(): any;
+    listen(): any;
     on(event: ConnectorEvent, handler: () => any): void;
     emit(event: ConnectorEvent, data: any): void;
 }
