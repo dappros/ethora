dd982b6f64661b305841e8c4b9869a88f4b548f8
Anton Kolot <antvelkol@gmail.com>
2023-02-13 12:22:29 +0200

    Updated connector logic, added xmpp initialization and message receiving

--- bots/botTemplate/src/connectors/Connector.ts
+++ bots/botTemplate/src/connectors/Connector.ts
@@ -1,23 +1,41 @@
 import EventEmitter = require("events");
-import {IConnector} from "./IConnector";
+import {IConnector, ConnectorEvent } from "./IConnector";
 import {IUser} from "../core/IUser";
-import {IMessage} from "../core/IMessage";
+import {IMessage, IMessageProps, MessageSender} from "../core/IMessage";
+import XmppClient from "../client/XmppClient";
+import ApplicationAPI from "../api/ApplicationAPI";
+import {IAuthorization} from "../api/IAuthorization";
+import {Message} from "../core/Message";
+import {DOMAIN} from "../Config";
 
 export default class Connector extends EventEmitter implements IConnector {
-    static _getUser(): IUser {
-        return {jid: 'user', firstName: 'UserFirst', lastName: 'UserLast'};
-    }
+    username: string;
+    password: string;
+    stanza: any;
+    botAuthData: IAuthorization;
 
-    getConnectorName(): string {
-        return 'console';
+    constructor(username: string, password: string) {
+        super();
+        this.username = username;
+        this.password = password;
     }
 
     getUniqueSessionKey() {
-        return this.getConnectorName();
+        return this.stanza.attrs.from.split("/").pop() + DOMAIN;
     }
 
-    async getUser(): Promise<IUser> {
-        return Promise.resolve(Connector._getUser());
+    getUser(): IUser {
+        if (!this.stanza || !this.stanza.getChild('data')) {
+            return;
+        }
+
+        return {
+            userJID: String(this.stanza.getChild('data').attrs.senderJID),
+            firstName: String(this.stanza.getChild('data').attrs.senderFirstName),
+            lastName: String(this.stanza.getChild('data').attrs.senderLastName),
+            photoURL: String(this.stanza.getChild('data').attrs.photoURL),
+            walletAddress: String(this.stanza.getChild('data').attrs.senderWalletAddress)
+        }
     }
 
     async send(message: IMessage) {
@@ -27,32 +45,48 @@ export default class Connector extends EventEmitter implements IConnector {
         return Promise.resolve();
     }
 
-    // listen() {
-    //     /* Создаем интерфейс для чтения stdin потока */
-    //     const rl = readline.createInterface(process.stdin, process.stdout);
-    //
-    //     rl.on('line', (line: string = '') => {
-    //         /* Если пользователь ввел quit, завершаем работу бота */
-    //         if (line.toLowerCase() === 'quit') {
-    //             rl.close();
-    //             process.exit();
-    //         }
-    //
-    //         const msg = new Message({
-    //             rawData: { text: line },
-    //             user: ConsoleConnector._getUser(),
-    //             sessionKey: this.getUniqueSessionKey(),
-    //             sender: MessageSender.user
-    //         });
-    //
-    //         /* Эммитим созданное сообщение (Bot случает событие receiveMessage,
-    //            поэтому сразу после вызыва начинает обрабатывать сообщение */
-    //         this.emit(ConnectorEvent.receiveMessage, msg);
-    //     });
-    //
-    //     return this;
-    // }
+    collectMessage(): IMessageProps {
+        return {
+            data: {
+                xmlns: String(this.stanza.getChild('data').attrs.xmlns),
+                isSystemMessage: toBooleanType(this.stanza.getChild('data').attrs.isSystemMessage),
+                tokenAmount: Number(this.stanza.getChild('data').attrs.tokenAmount),
+                receiverMessageId: Number(this.stanza.getChild('data').attrs.receiverMessageId),
+                mucname: String(this.stanza.getChild('data').attrs.mucname),
+                roomJid: String(this.stanza.getChild('data').attrs.roomJid),
+                isReply: toBooleanType(this.stanza.getChild('data').attrs.isReply),
+                mainMessageText: String(this.stanza.getChild('data').attrs.mainMessageText),
+                mainMessageId: String(this.stanza.getChild('data').attrs.mainMessageId),
+                mainMessageUserName: String(this.stanza.getChild('data').attrs.mainMessageUserName),
+                push: toBooleanType(this.stanza.getChild('data').attrs.push)
+            },
+            message: String(this.stanza.getChild('body').getText()),
+            user: this.getUser(),
+            sessionKey: this.getUniqueSessionKey(),
+            sender: this.botAuthData.data.jid ===  this.getUniqueSessionKey() ? MessageSender.bot : MessageSender.user
+        }
+    }
+
+    listen() {
+        const API = new ApplicationAPI();
+        API.userAuthorization(this.username, this.password).then(botAuthData => {
+            //Initializing XMPP Client
+            XmppClient.init(botAuthData.data.jid, botAuthData.data.xmppPassword);
+            this.botAuthData = botAuthData;
+            //Listen for incoming messages and redirect them to a bot
+            XmppClient.client.on("stanza", (stanza) => {
+                //If there is "data" in the incoming "stanza", then these are message and the bot is processing it.
+                if (stanza.getChild('body')) {
+                    this.stanza = stanza;
+                    const receivedMessage = new Message(this.collectMessage());
+                    this.emit(ConnectorEvent.receiveMessage, receivedMessage);
+                }
+            });
+        })
+        return this;
+    }
 }
 
-// const Teset = new Connector();
-// Teset.
+const toBooleanType = (text: string): boolean => {
+    return text.replace(/^\s+|\s+$/g, "").toLowerCase() === "true";
+};
