8f1f3aa3c57b20b366416f8359d6062ea11d066e
Anton Kolot <antvelkol@gmail.com>
2023-02-14 10:41:43 +0200

    Updated the interface of the message sending method

--- bots/botTemplate/src/connectors/IConnector.ts
+++ bots/botTemplate/src/connectors/IConnector.ts
@@ -1,6 +1,7 @@
 import { IUser } from '../core/IUser';
 import { IMessage } from '../core/IMessage';
 import {IAuthorization} from "../api/IAuthorization";
+import {IKeyboard} from "../client/types/IKeyboard";
 
 export enum ConnectorEvent {
     receiveMessage = 'receiveMessage'
@@ -13,7 +14,7 @@ export interface IConnector {
     botAuthData: IAuthorization;
     getUniqueSessionKey(): string;
     getUser(): IUser;
-    send(message: IMessage, user: IUser, options: any): Promise<void>;
+    send(message: string, keyboard?: IKeyboard): Promise<void>;
     listen(): any;
     on(event: ConnectorEvent, handler: () => any): void;
     emit(event: ConnectorEvent, data: any): void;
