78fdd0e60747ef151290481548ebf6bdc37bce8d
Anton Kolot <antvelkol@gmail.com>
2023-02-16 13:42:28 +0200

    Updated configuration file directory and logic

---
+++ bots/botTemplate/src/config/Config.ts
@@ -0,0 +1,62 @@
+import {IConfig, IConfigData} from "./IConfig";
+import Logger from "../utils/Logger";
+
+class Config implements IConfig {
+    private data: {
+        isProduction: boolean;
+        baseDomain: string;
+        botName: string;
+        conferenceDomain: string;
+        domain: string;
+        service: string;
+        botImg: string;
+        apiDomain: string;
+        tokenJWT: string;
+    }
+
+    init(botName: string, tokenJWT: string, isProduction?: boolean, botImg?: string): void {
+        if (!botName) {
+            throw Logger.error(new Error('botName (or username) is a required parameter to set the bot configuration.'));
+        }
+
+        if (!tokenJWT) {
+            throw Logger.error(new Error('tokenJWT is a required parameter to set the bot configuration.'));
+        }
+
+        const baseDomain = isProduction ? "dxmpp.com" : "dev.dxmpp.com";
+        this.data = {
+            isProduction: isProduction ? isProduction : false,
+            baseDomain: baseDomain,
+
+            conferenceDomain: '@conference.' + baseDomain,
+            domain: "@" + baseDomain,
+            service: `wss://${baseDomain}:5443/ws`,
+
+            botName: botName,
+            botImg: botImg ? botImg : 'https://cdn-icons-png.flaticon.com/512/9690/9690648.png',
+            apiDomain: isProduction ? "https://app.dappros.com/v1/" : "https://app-dev.dappros.com/v1/",
+            tokenJWT: tokenJWT
+        }
+        Logger.info(this.data.botName + ' - Bot configuration data successfully set.')
+        return;
+    }
+
+    getData(): IConfigData {
+        if (!this.data) {
+            return {
+                isProduction: false,
+                baseDomain: "dxmpp.com",
+                botName: "EthoraBot",
+                conferenceDomain: '@conference.dxmpp.com',
+                domain: "@dxmpp.com",
+                service: `wss://dxmpp.com:5443/ws`,
+                botImg: 'https://cdn-icons-png.flaticon.com/512/9690/9690648.png',
+                apiDomain: "https://app-dev.dappros.com/v1/",
+                tokenJWT: ''
+            };
+        }
+        return this.data;
+    }
+}
+
+export default new Config();
