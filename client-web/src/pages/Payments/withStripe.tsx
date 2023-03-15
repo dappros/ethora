f77a795bd0727ccb0b829e228e04c4f7b08db881
Mykhailo Mohyliuk <mykhailo@Mihajlos-MacBook-Air.local>
2023-03-15 09:27:16 +0200

    loading

--- client-web/src/pages/Payments/withStripe.tsx
+++ client-web/src/pages/Payments/withStripe.tsx
@@ -1,13 +1,17 @@
+import { Typography } from "@mui/material";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React, { useEffect, useMemo, useState } from "react";
 import { useHistory } from "react-router";
+import { FullPageSpinner } from "../../components/FullPageSpinner";
 import { config } from "../../config";
 import { useSnackbar } from "../../context/SnackbarContext";
 import { httpWithAuth } from "../../http";
 
 export const withStripe = (Component: React.ComponentType) => () => {
   const [clientSecret, setClientSecret] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const { showSnackbar } = useSnackbar();
   const history = useHistory();
   const stripePromise = useMemo(
@@ -20,6 +24,7 @@ export const withStripe = (Component: React.ComponentType) => () => {
   );
 
   const getClientSecret = async () => {
+    setLoading(true);
     try {
       const res = await httpWithAuth().get("/create-checkout-intent");
       setClientSecret(res.data.client_secret);
@@ -27,10 +32,23 @@ export const withStripe = (Component: React.ComponentType) => () => {
       showSnackbar("error", "Cannot get payment intent");
       history.push("/");
     }
+    setLoading(false);
   };
   useEffect(() => {
     getClientSecret();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (loading) {
+    return <FullPageSpinner />;
+  }
+  if (!clientSecret) {
+    return (
+      <Typography sx={{ textAlign: "center", fontWeight: "bold" }}>
+        Cannot get payment intent
+      </Typography>
+    );
+  }
 
   return (
     <Elements stripe={stripePromise} options={stripeOptions}>
