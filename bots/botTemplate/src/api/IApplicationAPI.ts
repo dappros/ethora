74b2423b53af7244b7ddc59808eccc876fbc56a5
Anton Kolot <antvelkol@gmail.com>
2023-03-20 15:10:06 +0200

    Balance interface moved separately

--- bots/botTemplate/src/api/IApplicationAPI.ts
+++ bots/botTemplate/src/api/IApplicationAPI.ts
@@ -1,19 +1,21 @@
 import {IAuthData, IAuthorization} from "./IAuthorization";
 
+export interface IBalance {
+    tokenName: string;
+    tokenSymbol: string;
+    balance: string;
+    contractAddress: string;
+}
+
 export interface IWalletBalance {
     success: boolean;
-    balance: {
-        tokenName: string;
-        tokenSymbol: string;
-        balance: string;
-        contractAddress: string;
-    }[];
+    balance: IBalance[];
     defaultToken: string;
     nfmtContracts: any;
 }
 
 
-export interface IApplicationAPI{
+export interface IApplicationAPI {
     userAuthorization(username: string, password: string): Promise<IAuthorization>;
     userRegistration(username: string, password: string): Promise<IAuthData>;
     getBalance(): Promise<any>;
