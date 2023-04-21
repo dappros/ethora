a16ee1683a4c77e80cfb18698fe8a13b91bcd59d
Anton Kolot <antvelkol@gmail.com>
2023-04-21 14:07:50 +0300

    Bot logic added

--- bots/botTemplate/bots/gptBot/src/index.js
+++ bots/botTemplate/bots/gptBot/src/index.js
@@ -1,4 +1,30 @@
 const { Bot } = require('../../../lib');
 const { botInitData } = require("./config/Config");
-const bot = new Bot(botInitData);
+const { runCompletion } = require("./utils/Openai");
 
+const bot = new Bot(botInitData().botData);
+
+bot.use("Start", async (context) => {
+
+    await context.session.sendTextMessage("Hello!\n" +
+        "Send me all your questions and I'll try to answer them.");
+
+    // Move to the next step
+    context.stepper.nextUserStep();
+});
+
+bot.use(async (context) => {
+
+    const indexOfSpace = context.message.getText().indexOf(' ');
+    let clearMessage = context.message.getText().slice(indexOfSpace + 1);
+    if (!clearMessage.endsWith('.')) {
+        clearMessage += '.'
+    }
+
+    runCompletion(clearMessage).then(result => {
+        return context.session.sendTextMessage(result);
+    }).catch(error => {
+        console.log("Error: ", error)
+    })
+
+}, 1);
