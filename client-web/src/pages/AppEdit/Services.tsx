3b036a8e5e991b6e4d0f01f7e50e6b758578ba5a
Mykhailo Mohyliuk <mogilyk32@gmail.com>
2023-05-26 10:18:01 +0300

    #425 added firebase fields

--- client-web/src/pages/AppEdit/Services.tsx
+++ client-web/src/pages/AppEdit/Services.tsx
@@ -3,6 +3,9 @@ import { Box, Button, TextField, Typography } from "@mui/material";
 import * as http from "../../http";
 import { useParams } from "react-router";
 import { useStoreState } from "../../store";
+import { useFormik } from "formik";
+import { useSnackbar } from "../../context/SnackbarContext";
+import { LoadingButton } from "@mui/lab";
 
 export interface IServices {}
 
@@ -25,11 +28,12 @@ export const Services: React.FC<IServices> = ({}) => {
   });
   const { appId } = useParams<{ appId: string }>();
   const app = useStoreState((s) => s.apps.find((app) => app._id === appId));
+  const updateApp = useStoreState((s) => s.updateApp);
 
   const fileRef = useRef<HTMLInputElement>(null);
+  const { showSnackbar } = useSnackbar();
   const uploadCertificate = async (e: React.ChangeEvent<HTMLInputElement>) => {
     setLoading(true);
-    console.log(e);
     try {
       const fd = new FormData();
       fd.append("files", e.target.files[0]);
@@ -40,6 +44,75 @@ export const Services: React.FC<IServices> = ({}) => {
     }
     setLoading(false);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      REACT_APP_FIREBASE_API_KEY: app.REACT_APP_FIREBASE_API_KEY,
+      REACT_APP_FIREBASE_AUTH_DOMAIN: app.REACT_APP_FIREBASE_AUTH_DOMAIN,
+      REACT_APP_FIREBASE_PROJECT_ID: app.REACT_APP_FIREBASE_PROJECT_ID,
+      REACT_APP_FIREBASE_STORAGE_BUCKET: app.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      REACT_APP_FIREBASE_MESSAGING_SENDER_ID: app.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      REACT_APP_FIREBASE_APP_ID: app.REACT_APP_FIREBASE_APP_ID,
+      REACT_APP_FIREBASE_MEASURMENT_ID: app.REACT_APP_FIREBASE_MEASURMENT_ID,
+    },
+    validate: (values) => {
+      const errors: Record<string, string> = {};
+
+      return errors;
+    },
+    onSubmit: async (
+      {
+        REACT_APP_FIREBASE_API_KEY,
+        REACT_APP_FIREBASE_AUTH_DOMAIN,
+        REACT_APP_FIREBASE_PROJECT_ID,
+        REACT_APP_FIREBASE_STORAGE_BUCKET,
+        REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        REACT_APP_FIREBASE_APP_ID,
+        REACT_APP_FIREBASE_MEASURMENT_ID,
+      },
+      { setSubmitting }
+    ) => {
+      const data = new FormData();
+      REACT_APP_FIREBASE_API_KEY &&
+        data.append("REACT_APP_FIREBASE_API_KEY", REACT_APP_FIREBASE_API_KEY);
+      REACT_APP_FIREBASE_AUTH_DOMAIN &&
+        data.append(
+          "REACT_APP_FIREBASE_AUTH_DOMAIN",
+          REACT_APP_FIREBASE_AUTH_DOMAIN
+        );
+      REACT_APP_FIREBASE_PROJECT_ID &&
+        data.append(
+          "REACT_APP_FIREBASE_PROJECT_ID",
+          REACT_APP_FIREBASE_PROJECT_ID
+        );
+      REACT_APP_FIREBASE_STORAGE_BUCKET &&
+        data.append(
+          "REACT_APP_FIREBASE_STORAGE_BUCKET",
+          REACT_APP_FIREBASE_STORAGE_BUCKET
+        );
+      REACT_APP_FIREBASE_MESSAGING_SENDER_ID &&
+        data.append(
+          "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+          REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+        );
+      REACT_APP_FIREBASE_APP_ID &&
+        data.append("REACT_APP_FIREBASE_APP_ID", REACT_APP_FIREBASE_APP_ID);
+      REACT_APP_FIREBASE_MEASURMENT_ID &&
+        data.append(
+          "REACT_APP_FIREBASE_MEASURMENT_ID",
+          REACT_APP_FIREBASE_MEASURMENT_ID
+        );
+      setSubmitting(true);
+      try {
+        const res = await http.updateAppSettings(appId, data);
+        updateApp(res.data.result)
+      } catch (error) {
+        showSnackbar("error", "Cannot save settings");
+        console.log({ error });
+      }
+      setSubmitting(false);
+    },
+  });
 
   return (
     <Box>
@@ -82,11 +155,133 @@ export const Services: React.FC<IServices> = ({}) => {
           // error={!!formik.touched.appUrl && !!formik.errors.appUrl}
         />
       </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_API_KEY"
+          name="REACT_APP_FIREBASE_API_KEY"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_API_KEY}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_API_KEY &&
+            !!formik.errors.REACT_APP_FIREBASE_API_KEY
+          }
+        />
+      </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_AUTH_DOMAIN"
+          name="REACT_APP_FIREBASE_AUTH_DOMAIN"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_AUTH_DOMAIN}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_AUTH_DOMAIN &&
+            !!formik.errors.REACT_APP_FIREBASE_AUTH_DOMAIN
+          }
+        />
+      </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_PROJECT_ID"
+          name="REACT_APP_FIREBASE_PROJECT_ID"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_PROJECT_ID}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_PROJECT_ID &&
+            !!formik.errors.REACT_APP_FIREBASE_PROJECT_ID
+          }
+        />
+      </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_STORAGE_BUCKET"
+          name="REACT_APP_FIREBASE_STORAGE_BUCKET"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_STORAGE_BUCKET}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_STORAGE_BUCKET &&
+            !!formik.errors.REACT_APP_FIREBASE_STORAGE_BUCKET
+          }
+        />
+      </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_MESSAGING_SENDER_ID"
+          name="REACT_APP_FIREBASE_MESSAGING_SENDER_ID"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_MESSAGING_SENDER_ID &&
+            !!formik.errors.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+          }
+        />
+      </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_APP_ID"
+          name="REACT_APP_FIREBASE_APP_ID"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_APP_ID}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_APP_ID &&
+            !!formik.errors.REACT_APP_FIREBASE_APP_ID
+          }
+        />
+      </Box>
+      <Box>
+        <TextField
+          sx={{ width: "100%" }}
+          margin="dense"
+          label="REACT_APP_FIREBASE_MEASURMENT_ID"
+          name="REACT_APP_FIREBASE_MEASURMENT_ID"
+          variant="outlined"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.REACT_APP_FIREBASE_MEASURMENT_ID}
+          error={
+            !!formik.touched.REACT_APP_FIREBASE_MEASURMENT_ID &&
+            !!formik.errors.REACT_APP_FIREBASE_MEASURMENT_ID
+          }
+        />
+      </Box>
       <Typography sx={{ fontSize: 12 }}>
         On Free plan, you use our default Ethora integrations. To publish to
         Appstore, you need to register your own accounts in these services and
         upgrade.
       </Typography>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <LoadingButton
+          loading={formik.isSubmitting}
+          disabled={formik.isSubmitting}
+          onClick={() => formik.handleSubmit()}
+          variant="contained"
+        >
+          Save
+        </LoadingButton>
+      </Box>
     </Box>
   );
 };
