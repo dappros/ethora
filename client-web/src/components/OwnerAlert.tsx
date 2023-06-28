7dc9e9401888b1ec8c9b88b0c1bf15f2fe7f420c
Mykhailo Mohyliuk <mogilyk32@gmail.com>
2023-06-28 22:13:48 +0300

    info tooltip #444

---
+++ client-web/src/components/OwnerAlert.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Alert, AlertTitle, IconButton, List, ListItem } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+export interface IOwnerAlert {
+  onClose: () => void;
+}
+
+export const OwnerAlert: React.FC<IOwnerAlert> = ({ onClose }) => {
+  return (
+    <Alert
+      severity={"info"}
+      action={
+        <IconButton
+          aria-label="close"
+          color="inherit"
+          size="small"
+          onClick={onClose}
+        >
+          <CloseIcon fontSize="inherit" />
+        </IconButton>
+      }
+    >
+      <AlertTitle>Apps</AlertTitle>
+      Here you can Create, Manage and View applications depending on your
+      permissions.
+      <List
+        dense
+        sx={{
+          listStyleType: "disc",
+          listStylePosition: "inside",
+        }}
+      >
+        <ListItem sx={{ display: "list-item" }}>
+          Create: use the green “+” icon.
+        </ListItem>
+        <ListItem sx={{ display: "list-item" }}>
+          Manage / View: click one of the available apps in the list.
+        </ListItem>
+      </List>
+    </Alert>
+  );
+};
--- client-web/src/pages/Owner/index.tsx
+++ client-web/src/pages/Owner/index.tsx
@@ -2,18 +2,25 @@ import Apps from "./Apps";
 import Box from "@mui/material/Box";
 import { Container } from "@mui/material";
 import UsersTable from "../../components/UsersTable/UsersTable";
+import { OwnerAlert } from "../../components/OwnerAlert";
+import { useState } from "react";
 
 export default function Owner() {
+  const [showInfoAlert, setShowInfoAlert] = useState(true);
+  const closeAlert = () => {
+    setShowInfoAlert(false);
+  };
   return (
     <div style={{ padding: "20px" }}>
       <Container maxWidth={false}>
+        {showInfoAlert && (
+          <Box>
+            <OwnerAlert onClose={closeAlert} />
+          </Box>
+        )}
         <Box style={{ marginTop: "20px" }}>
           <Apps />
         </Box>
-
-        {/* <Box style={{ marginTop: "20px" }}>
-          <Users />
-        </Box> */}
 
         <Box style={{ marginTop: "20px" }}>
           <UsersTable />
