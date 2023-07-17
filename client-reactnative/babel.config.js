1fe44943b72e1823a5d771afd8d7fb416d7df739
Mykhailo Mohyliuk <mogilyk32@gmail.com>
2023-07-17 11:18:42 +0300

    updated to walletConnect version 2

--- client-reactnative/babel.config.js
+++ client-reactnative/babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   "plugins": [
     'react-native-reanimated/plugin',
+    ['@babel/plugin-transform-flow-strip-types', {loose: true}], //https://github.com/facebook/react-native/issues/29084#issuecomment-1030732709
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
+    ['@babel/plugin-proposal-private-methods', {loose: true}],
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
   ],
   presets: ['module:metro-react-native-babel-preset'],
 };
--- client-reactnative/index.js
+++ client-reactnative/index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
+import './shim';
 import 'react-native-gesture-handler';
 
-import './shim';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
