ddf9a1051b5020a8e1d5dba9908e563f798d6a8c
Taras Filatov <phwizard@gmail.com>
2023-08-16 14:19:10 +0100

    Update tags.js

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,7 @@
 module.exports = [
   {
     name: 'Apps',
+    description: 'Managing Apps and their settings. \n /n ℹ️ Apps are entities that you create for your specific projects. Apps have certain setttings and properties including visual branding and User default settings that allow you to tailor the experience to the needs of your Users and your project. Normally each app has its own mobile build (iOS, Android client) distributed via Appstore under your name and a web client distributed via your own custom URL such as app.yourdomain.com.'
   },
   {
     name: 'Explorer',
