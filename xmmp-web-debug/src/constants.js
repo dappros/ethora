0facb750fd4c066de3d08c8ca48f77e08ae10385
transkarpation <57521389+transkarpation@users.noreply.github.com>
2023-11-02 10:38:42 +0200

    Delete xmmp-web-debug directory

--- xmmp-web-debug/src/constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  XMPP_SERVICE: 'wss://dev.dxmpp.com:5443/ws',
-  XMPP_CONFERENCE: 'conference.dev.dxmpp.com'
-}
-
-// module.exports = {
-//   XMPP_SERVICE: 'wss://xmpp.qa.ethoradev.com:5443/ws',
-//   XMPP_CONFERENCE: 'conference.xmpp.qa.ethoradev.com'
-// }
