6e61bb5eceb4181aa5598e0d17779b3ef08ea613
Taras Filatov <phwizard@gmail.com>
2024-03-14 10:51:52 +0000

    Update chatSlice.ts

--- experimental-chat-component/src/store/chatSlice.ts
+++ experimental-chat-component/src/store/chatSlice.ts
@@ -1,3 +1,13 @@
+// 
+//  We need a multi-room persistent store with following features:
+//  Store or refer - UserID 
+//  Upon login, we check if current user ID = stored UserID. If not, we clean/reset the whole store.
+//  If user ID matches then we reuse the cached store.
+//  
+//  [ room JID ] [ messageID = timestamp ] [ message stanza text ] [ message type = system or default ] 
+//
+//
+
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IMessage, UserType } from "../types/types";
 
