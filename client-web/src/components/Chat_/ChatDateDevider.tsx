014f555d387efca6351efc3148690dbe9e56056a
Borys <transkarpation@gmail.com>
2024-05-01 09:37:06 +0300

    today, yesterday for the ChatDateDevider

--- client-web/src/components/Chat_/ChatDateDevider.tsx
+++ client-web/src/components/Chat_/ChatDateDevider.tsx
@@ -1,13 +1,35 @@
+import {DateTime} from 'luxon'
+
 type Props = {
     date: string
 }
 
-const ONE_DAY = 24 * 60 * 60 * 1000;
+const dateFormatter = (date: DateTime) => {
+    return date.toFormat('cccc, dd LLLL yyyy')
+}
 
-export const ChatDateDevider = ({date}: Props) => {
+const isTheSameDate = (d1: DateTime, d2: DateTime) => {
+    return ( d1.hasSame(d2, 'day') && d1.hasSame(d2, 'month') && d1.hasSame(d2, 'year') )
+}
+
+export const ChatDateDevider = (props: Props) => {
+    const date = DateTime.fromMillis(Number(props.date))
+    const today = DateTime.now()
+    const yesterday = DateTime.now().minus({days: 1})
+
+    let text = dateFormatter(date);
+
+    if (isTheSameDate(date, today)) {
+        text = "Today"
+    }
+
+    if (isTheSameDate(date, yesterday)) {
+        text = "Yesterday"
+    }
+
     return (
         <div>
-            ChatDateDivider {date}
+            <span>{ text }</span>
         </div>
     )
 }
