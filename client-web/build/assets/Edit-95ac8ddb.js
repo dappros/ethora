1e0b9a989e60aef409015889e6418cd6d097420c
Borys <transkarpation@gmail.com>
2024-06-27 08:59:11 +0300

    gitignore

---
+++ client-web/build/assets/Edit-95ac8ddb.js
@@ -0,0 +1,1 @@
+import{ag as a,ah as t,l as r}from"./index-451a7b86.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var l=e.default=void 0,d=u(a()),i=r,o=(0,d.default)((0,i.jsx)("path",{d:"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"}),"Edit");l=e.default=o;export{l as d};
