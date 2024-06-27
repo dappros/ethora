c8a3ba1330ad321d127804cae80c4e3564739301
Borys <transkarpation@gmail.com>
2024-06-27 09:01:08 +0300

    remove

--- client-web/build/assets/index-da19c86c.js
+++
@@ -1,5 +0,0 @@
-import{aF as l}from"./index-451a7b86.js";var c={exports:{}};/*!
-	Copyright (c) 2018 Jed Watson.
-	Licensed under the MIT License (MIT), see
-	http://jedwatson.github.io/classnames
-*/(function(i){(function(){var f={}.hasOwnProperty;function t(){for(var n=[],e=0;e<arguments.length;e++){var s=arguments[e];if(s){var o=typeof s;if(o==="string"||o==="number")n.push(s);else if(Array.isArray(s)){if(s.length){var a=t.apply(null,s);a&&n.push(a)}}else if(o==="object"){if(s.toString!==Object.prototype.toString&&!s.toString.toString().includes("[native code]")){n.push(s.toString());continue}for(var r in s)f.call(s,r)&&s[r]&&n.push(r)}}}return n.join(" ")}i.exports?(t.default=t,i.exports=t):window.classNames=t})()})(c);var p=c.exports;const m=l(p);export{m as c};
